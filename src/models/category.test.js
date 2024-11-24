fabe7876f3703401f26fb86d9cd6fc6630fb41b8
Tamás Tóth <tomjtoth@gmail.com>
2024-11-24 10:15:28 +0200

    dummy test implemented

---
+++ src/models/category.test.js
@@ -0,0 +1,5 @@
+const { notEqual, strictEqual, deepStrictEqual } = require("node:assert");
+const { it, beforeEach, after, describe } = require("node:test");
+it.only("adsf", async () => {
+  return strictEqual(true, true);
+});
