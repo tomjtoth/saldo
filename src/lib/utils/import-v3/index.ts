66c0a52e56c453fc98af331bf8bd9e869e7da24b
Tamás Tóth <tomjtoth@gmail.com>
2025-05-25 14:27:30 +0300

    importV3 to use fn atomic

--- src/lib/utils/import-v3/index.ts
+++ src/lib/utils/import-v3/index.ts
@@ -3,53 +3,35 @@
 import { CSV_PATH } from "../config";
 import { parseCSV, parseData, TCsvRow, TDBData } from "./parsers";
 import {
-  db,
-
-  // types
-  TRevision,
-  TUser,
-  TCategory,
-  TReceipt,
-  TItem,
-  TItemShare,
-
-  // tables
   Revision,
   User,
   Category,
   Receipt,
   Item,
   ItemShare,
+  atomic,
 } from "@/lib/models";
 
 export async function importV3() {
   const read = await parseCSV(CSV_PATH);
-  const parsed = await parseData(read);
+  const parsed = parseData(read);
   return await insertData(parsed);
 }
 
 async function insertData(data: TDBData) {
-  return new Promise<void>(async (done, failed) => {
-    // drop ALL imported data
-    await Revision.truncate();
+  // drop ALL imported data
+  await Revision.truncate();
 
-    const transaction = await db.transaction();
-
-    try {
-      await Revision.bulkCreate(data.revisions, { transaction });
-      await User.bulkCreate(data.users, { transaction });
-      await Category.bulkCreate(data.categories, { transaction });
-      await Receipt.bulkCreate(data.receipts, { transaction });
-      await Item.bulkCreate(data.items, { transaction });
-      await ItemShare.bulkCreate(data.itemShares, { transaction });
-
-      await transaction.commit();
-      console.log("\n\tSUCCESSFULLY IMPORTED V3\n");
-      done();
-    } catch (err) {
-      await transaction.rollback();
-      console.error(err);
-      failed(err);
-    }
+  const res = await atomic("Importing V3", async (transaction) => {
+    return Promise.all([
+      Revision.bulkCreate(data.revisions, { transaction }),
+      User.bulkCreate(data.users, { transaction }),
+      Category.bulkCreate(data.categories, { transaction }),
+      Receipt.bulkCreate(data.receipts, { transaction }),
+      Item.bulkCreate(data.items, { transaction }),
+      ItemShare.bulkCreate(data.itemShares, { transaction }),
+    ]);
   });
+
+  console.log(res);
 }
