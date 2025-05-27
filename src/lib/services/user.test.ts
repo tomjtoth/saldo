b55432dc1c9b2ccd7551a703203be9e8bb6d11bb
Tamás Tóth <tomjtoth@gmail.com>
2025-05-27 18:33:02 +0300

    added services/users.test.ts

---
+++ src/lib/services/user.test.ts
@@ -0,0 +1,24 @@
+import { describe, it, expect, beforeEach } from "vitest";
+
+import { Revision, User } from "../models";
+import { addUser } from "./user";
+import { VALID_USER_DATA } from "../test_helpers";
+
+describe("addUser", () => {
+  beforeEach(async () => {
+    await Revision.truncate();
+    await User.truncate();
+  });
+
+  it("works with proper attributes", async () => {
+    await addUser(VALID_USER_DATA);
+    const users = await User.findAll({});
+    const revisions = await Revision.findAll({});
+
+    expect(users).to.have.length(1);
+    expect(revisions).to.have.length(1);
+
+    expect(users[0].revId).toEqual(1);
+    expect(revisions[0].revBy).toEqual(1);
+  });
+});
