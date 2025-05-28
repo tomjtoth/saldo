0d3b86979a0617ef848b7aeea6f038b712382aba
Tamás Tóth <tomjtoth@gmail.com>
2025-05-28 20:56:00 +0300

    e2e test updated #deploy

--- src/components/import-button.tsx
+++ src/components/import-button.tsx
@@ -3,7 +3,9 @@ import { importV3 } from "@/lib/services/import-v3";
 export default function ImportButton() {
   return (
     <form action={importV3}>
-      <button type="submit">re-import V3</button>
+      <button id="import-btn" type="submit">
+        re-import V3
+      </button>
     </form>
   );
 }
