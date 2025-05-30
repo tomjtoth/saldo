ea18a64db997aa5cd8f079ed9ba019736b7d857e
Tamás Tóth <tomjtoth@gmail.com>
2025-05-30 15:06:38 +0300

    failing to use db in the signIn callback

--- src/auth.ts
+++ src/auth.ts
@@ -1,9 +1,23 @@
 import NextAuth from "next-auth";
+// import SequelizeAdapter from "@auth/sequelize-adapter";
 import Discord from "next-auth/providers/discord";
 import GitHub from "next-auth/providers/github";
 import Google from "next-auth/providers/google";
 import Spotify from "next-auth/providers/spotify";
 
+import { User } from "./lib/models";
+
 export const { handlers, signIn, signOut, auth } = NextAuth({
   providers: [Discord, GitHub, Google, Spotify],
+  // adapter: SequelizeAdapter(db),
+  callbacks: {
+    async signIn(params) {
+      const email = params.profile?.email!;
+      let user = await User.findOne({ where: { email } });
+      if (!user) {
+        user = await User.create({ email, name: "some1 new" });
+      }
+      return true;
+    },
+  },
 });
