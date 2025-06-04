a493b31a920fe1657b8c46849b4f514a2f5b02a1
Tamás Tóth <tomjtoth@gmail.com>
2025-06-04 14:04:39 +0300

    updated Home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,12 @@
-import Link from "next/link";
-
 export default function Home() {
   return (
     <>
       <h2>Work in Progress</h2>
-      <p>
-        This <s className="text-gray-300">is</s> will be a multi-user expense
-        tracker app. Go to the <Link href="/import">import view</Link>.
+
+      <p className="p-2">
+        This is a <b>WiP</b> multi-user expense tracker app.
       </p>
+
       {process.env.GIT_HASH && (
         <p>
           This version is deployed from:{" "}
