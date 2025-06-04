50f41bc981509c478054eb0496986936f5960a45
Tamás Tóth <tomjtoth@gmail.com>
2025-06-04 16:09:08 +0300

    added ToastContainer to RootLayout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import { ToastContainer } from "react-toastify";
 
 import "./globals.css";
 
@@ -33,6 +34,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <ToastContainer />
         <StoreProvider>
           <Canceler />
           <Sidepanel />
