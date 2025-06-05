dde70cb74fa0e3bd94017e864e394b5b24c30ff1
Tamás Tóth <tomjtoth@gmail.com>
2025-06-05 16:08:32 +0300

    adjusted params typing and awaits in categories/[id]/PUT

--- src/app/api/categories/[id]/route.ts
+++ src/app/api/categories/[id]/route.ts
@@ -4,30 +4,21 @@ import { auth } from "@/auth";
 import { currentUser } from "@/lib/services/user";
 import { updateCategory } from "@/lib/services/categories";
 
-type Params = {
-  params: {
-    id: string;
-  };
-};
+export async function PUT(
+  req: NextRequest,
+  { params }: { params: Promise<{ id: number }> }
+) {
+  const [data, sess] = await Promise.all([req.json(), auth()]);
+  if (!sess) return new Response(null, { status: 401 });
 
-export async function PUT(req: NextRequest, { params }: Params) {
-  const data = await req.json();
-
-  const sess = await auth();
-
-  if (!sess) {
-    return new Response(null, { status: 401 });
-  }
-
-  const user = await currentUser(sess);
-
-  const categoryId = parseInt(params.id);
+  const [{ id }, user] = await Promise.all([params, currentUser(sess)]);
 
   try {
-    const updated = await updateCategory(categoryId, user.id, data);
+    const updated = await updateCategory(id, user.id, data);
 
     return Response.json({ updated: updated!.get({ plain: true }) });
   } catch (err) {
-    return Response.error();
+    console.error(err);
+    return new Response(null, { status: 400 });
   }
 }
