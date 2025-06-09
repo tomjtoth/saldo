e792f0f6f68442af1155fe3f54e171d4dfd16d46
Tamás Tóth <tomjtoth@gmail.com>
2025-06-09 11:41:50 +0300

    fixed Categories page messed up by Header's transfer to srv-side

--- src/app/categories/[id]/page.tsx
+++ src/app/categories/[id]/page.tsx
@@ -1,5 +1,7 @@
+import Link from "next/link";
+
 import { Category, CategoryArchive, Revision, Status } from "@/lib/models";
-import { CliCategoryPage } from "@/components/categories";
+import Header from "@/components/header";
 
 export const dynamic = "force-dynamic";
 
@@ -24,5 +26,38 @@ export default async function CategoryPage({
 
   if (!cat) return new Response(null, { status: 404 });
 
-  return <CliCategoryPage cat={cat.get({ plain: true })} />;
+  return (
+    <>
+      <Header>
+        <div className="flex gap-2 items-center">
+          <Link href="/categories" className="hover:no-underline">
+            🔙
+          </Link>
+          <h2>Previous revisions</h2>
+        </div>
+      </Header>
+      <div className="p-2">
+        <table className="w-full text-center [&_th]:p-2 [&_td]:p-2">
+          <thead>
+            <tr>
+              <th>Description</th>
+              <th>Status</th>
+              <th>Revisioned 📅</th>
+            </tr>
+          </thead>
+          <tbody>
+            {[...cat.archives!, cat].toReversed().map((cat) => (
+              <tr key={`${cat.id}-${cat.revId!}`}>
+                <td>
+                  <code>{cat.description.replaceAll(" ", "·")}</code>
+                </td>
+                <td>{cat.Status!.description}</td>
+                <td>{cat.Revision!.revOn}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </>
+  );
 }
--- src/components/categories/index.tsx
+++ src/components/categories/index.tsx
@@ -1,2 +1,36 @@
-export * from "./listing";
-export * from "./individual";
+"use client";
+
+import { useEffect } from "react";
+
+import { TCliCategory, TStatus } from "@/lib/models";
+import { useAppDispatch, useAppSelector } from "@/lib/hooks";
+import { rCats } from "@/lib/reducers/categories";
+
+import CliCategoryAdder from "./adder";
+import CliCategoryRow from "./row";
+
+export function CliCategoriesPage({
+  categories,
+  statuses,
+}: {
+  categories: TCliCategory[];
+  statuses: TStatus[];
+}) {
+  const dispatch = useAppDispatch();
+  const cats = useAppSelector((s) => s.categories.cats);
+
+  useEffect(() => {
+    if (cats.length === 0)
+      dispatch(rCats.init({ cats: categories, stats: statuses }));
+  }, []);
+
+  return (
+    <div className="p-2 grid grid-cols-[auto_min-content] sm:grid-cols-[auto_min-content_auto_min-content] md:grid-cols-[auto_min-content_auto_min-content_auto_min-content] gap-2">
+      {cats.map((cat) => (
+        <CliCategoryRow key={cat.id} cat={cat} />
+      ))}
+
+      <CliCategoryAdder />
+    </div>
+  );
+}
