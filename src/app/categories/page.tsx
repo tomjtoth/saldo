d0e90b3d4e039b74bb7a56edf389ead62d7492db
Tamás Tóth <tomjtoth@gmail.com>
2025-06-10 22:30:46 +0300

    crudely filter visible categories based on user.id

--- src/app/categories/page.tsx
+++ src/app/categories/page.tsx
@@ -1,7 +1,9 @@
 import React from "react";
 import { col, fn, Op } from "sequelize";
 
-import { Category, CategoryArchive, Status } from "@/lib/models";
+import { Category, CategoryArchive, Revision, Status } from "@/lib/models";
+import { currentUser } from "@/lib/services/user";
+import { auth, signIn } from "@/auth";
 
 import Header from "@/components/header";
 import { CliCategoriesPage } from "@/components/categories";
@@ -10,10 +12,19 @@ import StoreProvider from "../StoreProvider";
 export const dynamic = "force-dynamic";
 
 export default async function CategoriesPage() {
+  const sess = await auth();
+  if (!sess) return signIn("", { redirectTo: "/categories" });
+
+  const user = await currentUser(sess);
   const [cats, statuses] = await Promise.all([
     Category.findAll({
       order: [[fn("LOWER", col("Category.description")), "ASC"]],
       include: [
+        {
+          model: Revision,
+          // TODO: get all partners of user
+          where: { revBy: { [Op.in]: [user.id] } },
+        },
         {
           model: CategoryArchive,
           as: "archives",
