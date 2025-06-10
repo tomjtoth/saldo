d849673f0f9b0e21ef50e72cb19705ad82527054
Tamás Tóth <tomjtoth@gmail.com>
2025-06-11 00:25:14 +0300

    moved GIT_HASH under the same div.p-2 on the root page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,16 +7,18 @@ export default function HomePage() {
         <h2>Work in Progress</h2>
       </Header>
 
-      <p className="p-2">
-        This is a <b>WiP</b> multi-user expense tracker app.
-      </p>
-
-      {process.env.GIT_HASH && (
+      <div className="p-2">
         <p>
-          This version is deployed from:{" "}
-          <code>{process.env.GIT_HASH.substring(0, 7)}</code>.
+          This is a <b>WiP</b> multi-user expense tracker app.
         </p>
-      )}
+
+        {process.env.GIT_HASH && (
+          <p>
+            This version is deployed from:{" "}
+            <code>{process.env.GIT_HASH.substring(0, 7)}</code>.
+          </p>
+        )}
+      </div>
     </>
   );
 }
