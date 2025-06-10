ea5cc35a845d88eada773e01c4b61d80089f4e51
Tamás Tóth <tomjtoth@gmail.com>
2025-06-10 18:07:30 +0300

    Header passes className to its middle div

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -5,11 +5,14 @@ import { PropsWithChildren } from "react";
 import Sidepanel from "./sidepanel";
 import UserMenu from "./user-menu";
 
-export default async function Header({ children }: PropsWithChildren) {
+export default async function Header({
+  children,
+  className: cn,
+}: PropsWithChildren & { className?: string }) {
   return (
     <header className="flex gap-2 p-2 items-center">
       <Sidepanel />
-      <div className="grow">{children}</div>
+      <div className={`grow${cn ? ` ${cn}` : ""}`}>{children}</div>
       <UserMenu />
     </header>
   );
