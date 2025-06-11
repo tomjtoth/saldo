6047e66004da518343d3b8034a57af59b9b33f0e
Tamás Tóth <tomjtoth@gmail.com>
2025-06-11 17:18:31 +0300

    WiP: receipts page #deploy

--- src/app/receipts/page.tsx
+++ src/app/receipts/page.tsx
@@ -1,15 +1,90 @@
-import Link from "next/link";
+import { col, fn, Op } from "sequelize";
 
+import { auth, signIn } from "@/auth";
+import StoreProvider from "@/app/StoreProvider";
+import {
+  Category,
+  Item,
+  Receipt,
+  ReceiptArchive,
+  Revision,
+  User,
+} from "@/lib/models";
+import { currentUser } from "@/lib/services/user";
+
+import CliReceiptAdder from "@/components/receipts";
 import Header from "@/components/header";
 
-export default function ReceiptsPage() {
+export const dynamic = "force-dynamic";
+
+export default async function ReceiptsPage() {
+  const session = await auth();
+  if (!session) return signIn("", { redirectTo: "/receipts" });
+
+  const user = await currentUser(session);
+
+  const [users, categories, receipts] = await Promise.all([
+    User.findAll({ order: ["name"] }),
+    Category.findAll({
+      where: { statusId: { [Op.eq]: 1 } },
+      order: [[fn("LOWER", col("description")), "ASC"]],
+    }),
+    Receipt.findAll({
+      order: [["paidOn", "DESC"]],
+      include: [
+        { model: Item, as: "items" },
+        {
+          model: Revision,
+          // TODO: get all partners of user
+          where: { revBy: { [Op.in]: [user.id] } },
+          include: [User],
+        },
+        {
+          model: ReceiptArchive,
+          as: "archives",
+          separate: true,
+          include: [{ model: Revision, include: [User] }],
+          limit: 1,
+        },
+      ],
+      limit: 200,
+    }),
+  ]);
+
   return (
-    <>
-      <Header>
+    <StoreProvider>
+      <Header className="flex items-center gap-2">
         <h2>Receipts</h2>
+        <CliReceiptAdder
+          {...{
+            users: users.map((u) => u.get({ plain: true })),
+            categories: categories.map((cat) => cat.get({ plain: true })),
+
+            paidBy: user.id,
+          }}
+        />
       </Header>
 
-      <Link href="/receipts/new">Add new receipt</Link>
-    </>
+      <div className="p-2 grid gap-2 grid-cols-[auto_auto_auto_auto] sm:grid-cols-8 lg:sm:grid-cols-12 2xl:sm:grid-cols-16">
+        {receipts.map((rcpt) => (
+          <div
+            key={rcpt.id}
+            className="p-1 border rounded grid gap-2 col-span-4 grid-cols-subgrid"
+          >
+            <div>🛍️ {rcpt.items?.length}</div>
+            <div>🗓️ {rcpt.paidOn}</div>
+
+            <div>
+              🧔{" "}
+              {rcpt.archives!.length > 0
+                ? rcpt.archives?.at(0)?.Revision?.User?.name
+                : rcpt.Revision?.User?.name}
+            </div>
+
+            <div>💸 {rcpt.User?.name}</div>
+          </div>
+        ))}
+      </div>
+    </StoreProvider>
   );
 }
--- src/components/receipts/index.tsx
+++ src/components/receipts/index.tsx
@@ -1,15 +1,12 @@
 "use client";
 
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 import { useAppDispatch, useAppSelector } from "@/lib/hooks";
-import {
-  initReceiptAdder,
-  setPaidBy,
-  setPaidOn,
-} from "@/lib/reducers/receipt-adder";
+import { rReceipts as red } from "@/lib/reducers/receipts";
 import { TCategory, TUser } from "@/lib/models";
 
+import Canceler from "../Canceler";
 import ItemRows from "./item-row";
 
 export type TCLiReceiptAdder = {
@@ -20,42 +17,55 @@ export type TCLiReceiptAdder = {
 
 export default function CliReceiptAdder(props: TCLiReceiptAdder) {
   const dispatch = useAppDispatch();
-  const paidOn = useAppSelector((s) => s.receiptAdder.paidOn);
-  const paidBy = useAppSelector((s) => s.receiptAdder.paidBy);
+  const paidOn = useAppSelector((s) => s.receipts.paidOn);
+  const paidBy = useAppSelector((s) => s.receipts.paidBy);
+
+  const [open, setOpen] = useState(false);
 
   useEffect(() => {
-    dispatch(initReceiptAdder(props));
+    dispatch(red.init(props));
   }, []);
 
   return (
     <>
-      <div>
-        <label htmlFor="paid-on">paid on:</label>
-        <input
-          required
-          id="paid-on"
-          type="date"
-          value={paidOn}
-          onChange={(ev) => dispatch(setPaidOn(ev.target.value))}
-        />
-      </div>
+      <button className="p-2 border rounded" onClick={() => setOpen(true)}>
+        ➕
+      </button>
+      {open && (
+        <>
+          <Canceler onClick={() => setOpen(false)} />
 
-      <div>
-        <label htmlFor="paid-by">paid by:</label>
-        <select
-          id="paid-by"
-          value={paidBy}
-          onChange={(ev) => dispatch(setPaidBy(ev.target.value))}
-        >
-          {props.users.map((u) => (
-            <option key={u.id} value={u.id}>
-              {u.name} ({u.email})
-            </option>
-          ))}
-        </select>
-      </div>
+          <div className="absolute left-1/2 top-1/2 -translate-1/2 w-8/10 h-8/10 bg-background border rounded p-2">
+            <div>
+              <label htmlFor="paid-on">paid on:</label>
+              <input
+                required
+                id="paid-on"
+                type="date"
+                value={paidOn}
+                onChange={(ev) => dispatch(red.setPaidOn(ev.target.value))}
+              />
+            </div>
 
-      <ItemRows />
+            <div>
+              <label htmlFor="paid-by">paid by:</label>
+              <select
+                id="paid-by"
+                value={paidBy}
+                onChange={(ev) => dispatch(red.setPaidBy(ev.target.value))}
+              >
+                {props.users.map((u) => (
+                  <option key={u.id} value={u.id}>
+                    {u.name} ({u.email})
+                  </option>
+                ))}
+              </select>
+            </div>
+
+            <ItemRows />
+          </div>
+        </>
+      )}
     </>
   );
 }
