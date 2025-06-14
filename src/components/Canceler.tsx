9f3ace5940ca880d496dc6cb87ce491a57220520
Tamás Tóth <tomjtoth@gmail.com>
2025-06-14 23:07:32 +0300

    Canceler dims with a 1sec transition on opening

--- src/components/Canceler.tsx
+++ src/components/Canceler.tsx
@@ -1,3 +1,7 @@
+"use client";
+
+import { useEffect, useState } from "react";
+
 export default function Canceler({
   onClick: callback,
   zIndex = 0,
@@ -5,14 +9,21 @@ export default function Canceler({
   onClick: () => void;
   zIndex?: number;
 }) {
+  const classes = "absolute top-0 left-0 h-full w-full canceler";
+
+  const [cn, setCN] = useState(classes);
+
+  useEffect(() => {
+    setCN(classes + " dimmed");
+  }, []);
+
   return (
     <div
-      className={`${
-        zIndex !== 0 ? `z-${zIndex} ` : ""
-      }absolute top-0 left-0 h-full w-full bg-background/50 backdrop-blur-xs`}
+      className={cn}
       onClick={(ev) => {
         if (ev.target === ev.currentTarget) callback();
       }}
+      style={{ zIndex }}
     />
   );
 }
