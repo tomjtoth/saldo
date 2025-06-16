5efeb6a3dedb4c0f269bd9c96f35d6ca0dd59dca
Tamás Tóth <tomjtoth@gmail.com>
2025-06-16 23:43:27 +0300

    impl a standalone Adder with Name and Description fields and custom handler

---
+++ src/components/name-descr-adder.tsx
@@ -0,0 +1,69 @@
+"use client";
+
+import { useState } from "react";
+
+import Canceler from "./canceler";
+
+type AdderHandler = ({
+  name,
+  description,
+}: {
+  name: string;
+  description: string;
+}) => Promise<boolean>;
+
+export default function NameDescrAdder({ handler }: { handler: AdderHandler }) {
+  const [visible, setVisible] = useState(false);
+  const [name, setName] = useState("");
+  const [description, setDescr] = useState("");
+
+  return (
+    <>
+      <button onClick={() => setVisible(true)}>Add new...</button>
+      {visible && (
+        <>
+          <Canceler onClick={() => setVisible(false)} />
+          <form
+            className={
+              "absolute top-1/2 left-1/2 -translate-1/2 p-2 " +
+              "rounded border bg-background " +
+              "grid gap-2 grid-cols-[min-width_min-width]"
+            }
+            onSubmit={async (ev) => {
+              ev.preventDefault();
+
+              const res = await handler({ name, description });
+
+              if (res) {
+                setName("");
+                setDescr("");
+                setVisible(false);
+              }
+            }}
+          >
+            <input
+              {...{
+                id: "category-adder",
+                type: "text",
+                className: "w-full min-w-25",
+                placeholder: "Name",
+                value: name,
+
+                onChange: (ev) => setName(ev.target.value),
+              }}
+            />
+
+            <button className="rounded border">💾</button>
+
+            <textarea
+              className="col-span-2 resize-none border rounded "
+              placeholder="Optional description..."
+              value={description}
+              onChange={(ev) => setDescr(ev.target.value)}
+            />
+          </form>
+        </>
+      )}
+    </>
+  );
+}
