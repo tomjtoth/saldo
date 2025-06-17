6859ee33031cd893c802d18d0162ed9ef35e0382
Tamás Tóth <tomjtoth@gmail.com>
2025-06-17 22:03:00 +0300

    added explanation on members status handling

--- src/components/groups/details/members/index.tsx
+++ src/components/groups/details/members/index.tsx
@@ -9,6 +9,11 @@ export default function Members({ group }: { group: TGroup }) {
   return (
     <>
       <h3>Current members</h3>
+      <p className="text-center">
+        Any active member can see any other member regardless of their status.
+        Banned members cannot access the group or see its members, unless
+        re-instated by an admin.
+      </p>
       <ul className="flex flex-wrap gap-2 justify-center *:p-1">
         {group.Users?.map((user) => (
           <Individual key={user.id} {...{ user, isAdmin, groupId: group.id }} />
