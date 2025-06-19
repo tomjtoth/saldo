93d8c12ea2a5b9ef5dcf076b3036a791535fe750
Tamás Tóth <tomjtoth@gmail.com>
2025-06-19 10:29:58 +0300

    fix group details overflowing horizontally

--- src/components/groups/details/index.tsx
+++ src/components/groups/details/index.tsx
@@ -18,7 +18,7 @@ export default function Details({ group }: { group: TGroup }) {
         "max-w-min sm:max-w-4/5 max-h-4/5 overflow-scroll " +
         "bg-background rounded border-2 " +
         (statusId === 1 ? "border-green-500" : "border-red-500") +
-        " p-2 flex flex-col items-center flex-wrap gap-2"
+        " p-2 flex flex-col items-center gap-2"
       }
     >
       <Title {...{ group, statusId, setStatusId }} />
