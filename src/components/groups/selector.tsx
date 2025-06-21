737dec13a1b77423018c3b5df5b732cff9142d32
Tamás Tóth <tomjtoth@gmail.com>
2025-06-21 19:37:17 +0300

    GroupSelector to use custom drop-down solution

--- src/components/groups/selector.tsx
+++ src/components/groups/selector.tsx
@@ -1,35 +1,72 @@
 "use client";
 
 import Link from "next/link";
+import { useRef, useState } from "react";
 
 import { TGroup } from "@/lib/models";
 import { useAppDispatch, useGroupSelector } from "@/lib/hooks";
 import { rCombined as red } from "@/lib/reducers";
 
+import Canceler from "../canceler";
+
 export default function GroupSelector({ fallback }: { fallback: TGroup[] }) {
   const dispatch = useAppDispatch();
 
+  const [visible, setVisible] = useState(false);
+  const [dropdownPos, setDropdownPos] = useState<{
+    left: number;
+    top: number;
+  } | null>(null);
   const rs = useGroupSelector(fallback);
+  const linkRef = useRef<HTMLAnchorElement>(null);
 
-  return rs.groups.length == 0 ? null : (
-    <>
-      <div className="inline-block">
-        <Link href={`/groups/${rs.groupId}`} className="absolute">
-          {rs.group?.name}
-        </Link>{" "}
-        <select
-          id="group-selector"
-          className="cursor-pointer -z-1"
-          value={rs.groupId}
-          onChange={(ev) => dispatch(red.setGroupId(Number(ev.target.value)))}
-        >
-          {rs.groups.map((group) => (
-            <option key={group.id} value={group.id}>
-              {group.name}
-            </option>
-          ))}
-        </select>
-      </div>
-    </>
+  const openDropdown = () => {
+    if (linkRef.current) {
+      const rect = linkRef.current.getBoundingClientRect();
+      setDropdownPos({ left: rect.left, top: rect.bottom });
+    }
+    setVisible(true);
+  };
+
+  if (rs.groups.length === 0) return null;
+
+  return (
+    <div className="inline-block">
+      <Link ref={linkRef} href={`/groups/${rs.groupId}`}>
+        {rs.group?.name}
+      </Link>
+
+      <button className="p-1! ml-2" onClick={openDropdown}>
+        <div className="-rotate-90">&lt;</div>
+      </button>
+
+      {visible && (
+        <Canceler onClick={() => setVisible(false)} blur={false}>
+          <div
+            style={{
+              left: dropdownPos?.left,
+              top: dropdownPos?.top,
+            }}
+            className={
+              "absolute flex flex-col gap-2 border bg-background " +
+              "cursor-default " +
+              "*:hover:bg-foreground *:hover:text-background *:p-1"
+            }
+          >
+            {rs.groups.map((group) => (
+              <div
+                key={group.id}
+                onClick={() => {
+                  dispatch(red.setGroupId(group.id));
+                  setVisible(false);
+                }}
+              >
+                {group.name}
+              </div>
+            ))}
+          </div>
+        </Canceler>
+      )}
+    </div>
   );
 }
