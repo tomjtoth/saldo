2b4a769a163111d9826b9ab8ae0627145bdf5f25
Tamás Tóth <tomjtoth@gmail.com>
2025-06-24 22:02:21 +0300

    Canceler allows to extend it's className

--- src/components/canceler.tsx
+++ src/components/canceler.tsx
@@ -3,16 +3,18 @@
 import { PropsWithChildren, useEffect, useState } from "react";
 
 export default function Canceler({
+  blur = true,
   onClick: callback,
   zIndex = 0,
-  blur = true,
+  className = "",
   children,
 }: PropsWithChildren & {
   blur?: boolean;
   onClick: () => void;
   zIndex?: number;
+  className?: string;
 }) {
-  const classes = "absolute top-0 left-0 h-full w-full canceler";
+  const classes = "absolute top-0 left-0 h-full w-full canceler " + className;
 
   const [cn, setCN] = useState(classes);
 
