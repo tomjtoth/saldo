9bdd390eb10080459f2ef0fb57a01dd0802df7e7
Tamás Tóth <tomjtoth@gmail.com>
2025-06-30 14:03:58 +0300

    using POST /api/import

---
+++ src/app/api/import/route.ts
@@ -0,0 +1,20 @@
+import { alreadyInProd, importV3 } from "@/lib/services/import-v3";
+
+export async function POST() {
+  if (await alreadyInProd())
+    return new Response(null, {
+      status: 403,
+      statusText: "already in production",
+    });
+
+  try {
+    const imported = await importV3();
+
+    return Response.json(imported);
+  } catch (err) {
+    return new Response(null, {
+      status: 400,
+      statusText: (err as Error).message,
+    });
+  }
+}
--- src/lib/services/import-v3/index.ts
+++ src/lib/services/import-v3/index.ts
@@ -13,6 +13,12 @@ import {
   Membership,
 } from "@/lib/models";
 
+export const alreadyInProd = async () => {
+  const user = await User.findByPk(1);
+
+  return !!user && user.email !== "user0@just.imported";
+};
+
 export async function importV3() {
   const read = await parseCSV(process.env.CSV_PATH || "data/saldo-v3.csv");
   const parsed = parseData(read);
@@ -20,22 +26,39 @@ export async function importV3() {
 }
 
 export async function insertData(data: TDBData) {
-  await atomic("importing from V3", async (transaction) => {
+  return await atomic("importing from V3", async (transaction) => {
     // drop ALL imported data
     await Revision.truncate({ transaction, cascade: true });
 
     // these 2 can be run in parallel
-    await Promise.all([
+    const [revisions, users] = await Promise.all([
       Revision.bulkCreate(data.revisions, { transaction }),
       User.bulkCreate(data.users, { transaction }),
     ]);
 
     // the rest are to be inserted in strict order
-    await Group.bulkCreate(data.groups, { transaction });
-    await Membership.bulkCreate(data.memberships, { transaction });
-    await Category.bulkCreate(data.categories, { transaction });
-    await Receipt.bulkCreate(data.receipts, { transaction });
-    await Item.bulkCreate(data.items, { transaction });
-    await ItemShare.bulkCreate(data.itemShares, { transaction });
+    const groups = await Group.bulkCreate(data.groups, { transaction });
+    const memberships = await Membership.bulkCreate(data.memberships, {
+      transaction,
+    });
+    const categories = await Category.bulkCreate(data.categories, {
+      transaction,
+    });
+    const receipts = await Receipt.bulkCreate(data.receipts, { transaction });
+    const items = await Item.bulkCreate(data.items, { transaction });
+    const itemShares = await ItemShare.bulkCreate(data.itemShares, {
+      transaction,
+    });
+
+    return {
+      revisions: revisions.length,
+      users: users.length,
+      groups: groups.length,
+      memberships: memberships.length,
+      categories: categories.length,
+      receipts: receipts.length,
+      items: items.length,
+      itemShares: itemShares.length,
+    };
   });
 }
