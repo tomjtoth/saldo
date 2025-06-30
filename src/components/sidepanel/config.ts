765139499d6335066da327794578ebeaaf838b23
Tamás Tóth <tomjtoth@gmail.com>
2025-06-30 14:03:10 +0300

    rm link to /imports from sidepanel

--- src/components/sidepanel/config.ts
+++ src/components/sidepanel/config.ts
@@ -5,7 +5,6 @@ type TLink = {
 
 export const LINKS: TLink[] = [
   { href: "/", label: "home" },
-  { href: "/import", label: "import from V3" },
   { href: "/groups" },
   { href: "/categories" },
   { href: "/receipts" },
