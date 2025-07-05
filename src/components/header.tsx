0a4df2fcec4628e0ff0f17160be3670dc8c39d7b
Tamás Tóth <tomjtoth@gmail.com>
2025-07-05 23:54:20 +0300

    Header fetches userMenu from CliCommonCx.Provider if not passed as prop

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -3,12 +3,18 @@
 import { PropsWithChildren, ReactNode } from "react";
 
 import Sidepanel from "./sidepanel";
+import { useUserMenu } from "./common-context";
 
 export default function Header({
   children,
   userMenu,
   className: cn = "",
-}: PropsWithChildren & { userMenu: ReactNode; className?: string }) {
+}: PropsWithChildren & {
+  userMenu?: ReactNode;
+  className?: string;
+}) {
+  if (!userMenu) userMenu = useUserMenu();
+
   return (
     <header className="flex gap-2 p-2 items-center">
       <Sidepanel />
