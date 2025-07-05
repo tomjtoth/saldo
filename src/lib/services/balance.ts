96e4dc585cdeebe5bbba55ade77c75c906fd8680
Tamás Tóth <tomjtoth@gmail.com>
2025-07-05 17:10:53 +0300

    minimize post-processing of getBalanceDataFor(uid)

--- src/lib/services/balance.ts
+++ src/lib/services/balance.ts
@@ -1,110 +1,77 @@
 import { QueryTypes } from "sequelize";
 
-import { db, User } from "@/lib/models";
+import { db } from "@/lib/models";
 import { TBalanceChartData } from "@/components/balance/chart";
-
-type TRelation = {
-  name: string;
-  temp: number;
-};
 
 export async function getBalanceDataFor(userId: number) {
   const rows: {
-    groupId: number;
-    date: number;
-    uid1: number;
-    uid2: number;
-    share: number;
+    id: number;
+    name: string;
+    balance: string;
   }[] = await db.query(
-    `SELECT
-      groupId,
-      paidOn AS date,
-      paidBy AS uid1,
-      paidTo AS uid2,
-      SUM(share) as share
-    FROM memberships ms
-    INNER JOIN consumption c ON ms.group_id = c.groupId
-    WHERE ms.user_id = ? AND paidBy != paidTo
-    GROUP BY groupId, paidOn, paidBy, paidTo
-    ORDER BY groupId, date`,
+    `WITH step1 AS (
+      SELECT
+        groupId AS gid,
+        g.name AS gName,
+        paidOn AS date,
+        MIN(paidBy, paidTo) AS uid1,
+        MAX(paidBy, paidTo) AS uid2,
+        SUM(share * CASE WHEN paidBy < paidTo THEN 1 ELSE -1 END) as share
+      FROM memberships ms
+      INNER JOIN consumption c ON ms.group_id = c.groupId
+      INNER JOIN groups g ON groupId = g.id
+      WHERE ms.user_id = ? AND paidBy != paidTo
+      GROUP BY groupId, paidOn, paidBy, paidTo
+      ORDER BY groupId, date
+    ),
 
-    { type: QueryTypes.SELECT, replacements: [userId] }
+    step2 AS (
+      SELECT 
+        gid, 
+        gName,
+        date,
+        CONCAT(u1.name, ' vs ', u2.name) AS relation,
+        SUM(share) AS share
+      FROM step1
+      INNER JOIN users u1 ON u1.id = step1.uid1
+      INNER JOIN users u2 on u2.id = step1.uid2
+      GROUP BY gid, date, relation
+    ),
+
+    step3 AS (
+      SELECT 
+        gid,
+        gName,
+        date,
+        relation,
+        SUM(share) OVER (
+          PARTITION BY gid, relation
+          ROWS BETWEEN UNBOUNDED PRECEDING AND CURRENT ROW
+        ) AS share
+      FROM step2
+      GROUP BY gid, date, relation
+    )
+
+    SELECT 
+    	gid AS id,
+      gName AS name,
+      JSON_OBJECT(
+        'relations',
+        JSON_GROUP_ARRAY(distinct relation), 
+        
+        'data', 
+        JSON_GROUP_ARRAY(JSON_OBJECT('date', date, relation, share))
+      ) AS balance
+    FROM step3
+    GROUP BY gid`,
+
+    { type: QueryTypes.SELECT, replacements: [userId], nest: true }
   );
 
-  const users = await User.findAll({ attributes: ["id", "name"] });
+  const data = rows.map(({ balance, ...group }) => ({
+    ...group,
+    balance: JSON.parse(balance) as TBalanceChartData,
+  }));
 
-  const dataBuffer: {
-    [groupId: number]: {
-      relations: {
-        [uid1: number]: {
-          [uid2: number]: TRelation;
-        };
-      };
-      data: {
-        [date: number]: {
-          [relation: string]: number;
-        };
-      };
-    };
-  } = {};
-
-  // TODO: replace this post-processing with a professional sequelize query
-  rows.forEach(({ groupId, date, uid1, uid2, share }) => {
-    if (!dataBuffer[groupId]) dataBuffer[groupId] = { relations: {}, data: {} };
-    const group = dataBuffer[groupId];
-
-    let isDir12 = false;
-    let relation: TRelation;
-
-    if (!!group.relations[uid2] && !!group.relations[uid2][uid1]) {
-      relation = group.relations[uid2][uid1];
-    } else {
-      isDir12 = true;
-      let loading = true;
-
-      if (!group.relations[uid1]) {
-        group.relations[uid1] = {};
-        loading = false;
-      }
-
-      if (!group.relations[uid1][uid2]) {
-        relation = {
-          temp: 0,
-          name: `${users.find((u) => u.id == Number(uid1))!.name} vs ${
-            users.find((u) => u.id == Number(uid2))!.name
-          }`,
-        };
-        group.relations[uid1][uid2] = relation;
-        loading = false;
-      }
-
-      if (loading) relation = group.relations[uid1][uid2];
-    }
-    relation = relation!;
-
-    if (!group.data[date]) group.data[date] = {};
-
-    group.data[date][relation.name] =
-      relation.temp + (isDir12 ? 1 : -1) * share;
-    relation.temp = group.data[date][relation.name];
-  });
-
-  return Object.fromEntries(
-    Object.entries(dataBuffer).map(([groupId, { data, relations }]) => [
-      groupId,
-      {
-        relations: Object.values(relations).reduce((all, uid1vals) => {
-          Object.values(uid1vals).forEach(({ name }) => {
-            all.push(name);
-          });
-
-          return all;
-        }, [] as string[]),
-        data: Object.entries(data).map(([date, relations]) => ({
-          date: Number(date),
-          ...relations,
-        })),
-      } as TBalanceChartData,
-    ])
-  );
+  return data;
 }
