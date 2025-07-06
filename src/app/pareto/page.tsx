c569277dc70d23a57a5ae5bf32002e5f50d17d3c
Tamás Tóth <tomjtoth@gmail.com>
2025-07-06 20:39:55 +0300

    app/pareto to use protectedPage

--- src/app/pareto/page.tsx
+++ src/app/pareto/page.tsx
@@ -1,39 +1,14 @@
-import { auth, signIn } from "@/auth";
-import { currentUser } from "@/lib/services/user";
 import { getParetoDataFor } from "@/lib/services/pareto";
 
-import UserMenu from "@/components/user-menu";
 import CliParetoPage from "@/components/pareto";
+import protectedPage from "@/lib/protectedPage";
 
 export const dynamic = "force-dynamic";
 
-export default async function ParetoPage({
-  params,
-}: {
-  params: Promise<{ groupId?: string }>;
-}) {
-  const { groupId } = await params;
-
-  const sess = await auth();
-  if (!sess)
-    return signIn("", {
-      redirectTo: groupId ? `/groups/${groupId}/pareto` : "/pareto",
-    });
-
-  const gidAsNum = Number(groupId);
-
-  const user = await currentUser(sess);
-  const pareto = await getParetoDataFor(user.id);
-
-  return (
-    <CliParetoPage
-      {...{
-        userMenu: <UserMenu />,
-
-        defaultGroupId: user.defaultGroupId,
-        groupId: isNaN(gidAsNum) ? undefined : gidAsNum,
-        ...pareto,
-      }}
-    />
-  );
-}
+export default async ({ params }: { params: { groupId?: string } }) =>
+  protectedPage({
+    params,
+    getData: getParetoDataFor,
+    children: <CliParetoPage />,
+    rewritePath: "/pareto",
+  });
