d9fa579012bd45538e0aa96a7045e68efc2e461c
Tamás Tóth <tomjtoth@gmail.com>
2025-07-06 20:40:15 +0300

    app/receipts to use protectedPage

--- src/app/receipts/page.tsx
+++ src/app/receipts/page.tsx
@@ -1,39 +1,15 @@
-import { auth, signIn } from "@/auth";
-import { currentUser } from "@/lib/services/user";
 import { getReceiptsDataFor } from "@/lib/services/receipt";
 
+import protectedPage, { TCoreParams } from "@/lib/protectedPage";
 import CliReceiptsPage from "@/components/receipts";
-import UserMenu from "@/components/user-menu";
 
 export const dynamic = "force-dynamic";
 
-export default async function ReceiptsPage({
-  params,
-}: {
-  params: Promise<{ groupId?: string }>;
-}) {
-  const { groupId } = await params;
-
-  const session = await auth();
-  if (!session)
-    return signIn("", {
-      redirectTo: groupId ? `/groups/${groupId}/receipts` : "/receipts",
-    });
-
-  const user = await currentUser(session);
-  const groups = await getReceiptsDataFor(user.id);
-  const gidAsNum = Number(groupId);
-
-  return (
-    <CliReceiptsPage
-      {...{
-        userMenu: <UserMenu />,
-
-        userId: user.id,
-        groupId: isNaN(gidAsNum) ? undefined : gidAsNum,
-        defaultGroupId: user.defaultGroupId,
-        groups: groups.map((group) => group.get({ plain: true })),
-      }}
-    />
-  );
+export default async function ReceiptsPage({ params }: TCoreParams) {
+  return protectedPage({
+    params,
+    getData: getReceiptsDataFor,
+    children: <CliReceiptsPage />,
+    rewritePath: "/receipts",
+  });
 }
