c3ac64d0dc82cf5e3d6ec6f27c3d36c741eff8d1
Tamás Tóth <tomjtoth@gmail.com>
2025-07-06 20:35:29 +0300

    app/balance to use protectedPage

--- src/app/balance/page.tsx
+++ src/app/balance/page.tsx
@@ -1,39 +1,14 @@
-import { auth, signIn } from "@/auth";
-import { currentUser } from "@/lib/services/user";
 import { getBalanceDataFor } from "@/lib/services/balance";
 
-import UserMenu from "@/components/user-menu";
+import protectedPage, { TCoreParams } from "@/lib/protectedPage";
 import CliBalancePage from "@/components/balance";
 
 export const dynamic = "force-dynamic";
 
-export default async function BalancePage({
-  params,
-}: {
-  params: Promise<{ groupId?: string }>;
-}) {
-  const { groupId } = await params;
-
-  const sess = await auth();
-  if (!sess)
-    return signIn("", {
-      redirectTo: groupId ? `/groups/${groupId}/balance` : "/balance",
-    });
-
-  const gidAsNum = Number(groupId);
-
-  const user = await currentUser(sess);
-  const groups = await getBalanceDataFor(user.id);
-
-  return (
-    <CliBalancePage
-      {...{
-        userMenu: <UserMenu />,
-
-        defaultGroupId: user.defaultGroupId,
-        groupId: isNaN(gidAsNum) ? undefined : gidAsNum,
-        groups,
-      }}
-    />
-  );
-}
+export default async ({ params }: TCoreParams) =>
+  protectedPage({
+    params,
+    getData: getBalanceDataFor,
+    children: <CliBalancePage />,
+    rewritePath: "/balance",
+  });
