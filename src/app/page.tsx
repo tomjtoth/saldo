f54c3693bbec4c53a67e6827cdae27946f7bd8f1
Tamás Tóth <tomjtoth@gmail.com>
2025-07-07 23:04:37 +0300

    #deploying fixed build error (HomePage was anonymous)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,24 +3,26 @@ import Header from "@/components/header";
 import HashParagraph from "@/components/hashParagraph";
 import ImportSection from "@/components/import";
 
-export default () => (
-  <RootDiv>
-    <Header>
-      <h2>Saldo</h2>
-    </Header>
+export default function HomePage() {
+  return (
+    <RootDiv>
+      <Header>
+        <h2>Saldo</h2>
+      </Header>
 
-    <div className="p-2">
-      <p>
-        This is a multi-user expense tracker app. Most revisions of records are
-        archived and can be viewed by users related to them, but deletion is not
-        supported only setting an{" "}
-        <span className="p-1 rounded border-2 border-red-500">INACTIVE</span>{" "}
-        status.
-      </p>
+      <div className="p-2">
+        <p>
+          This is a multi-user expense tracker app. Most revisions of records
+          are archived and can be viewed by users related to them, but deletion
+          is not supported only setting an{" "}
+          <span className="p-1 rounded border-2 border-red-500">INACTIVE</span>{" "}
+          status.
+        </p>
 
-      <HashParagraph />
+        <HashParagraph />
 
-      <ImportSection />
-    </div>
-  </RootDiv>
-);
+        <ImportSection />
+      </div>
+    </RootDiv>
+  );
+}
