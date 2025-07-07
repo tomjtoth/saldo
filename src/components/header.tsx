7bcc9e7f5df6a9fb241cf7d8e7a5b6305ecc2e75
Tamás Tóth <tomjtoth@gmail.com>
2025-07-07 14:42:46 +0300

    Header to rely on useRootDivCx solely while retrieving <UserMenu>

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,25 +1,23 @@
 "use client";
 
-import { PropsWithChildren, ReactNode } from "react";
+import { PropsWithChildren } from "react";
 
 import Sidepanel from "./sidepanel";
 import { useRootDivCx } from "./rootDiv/clientSide";
 
 export default function Header({
   children,
-  userMenu: userMenuProps,
   className: cn = "",
 }: PropsWithChildren & {
-  userMenu?: ReactNode;
   className?: string;
 }) {
-  const userMenuCx = useRootDivCx().userMenu;
+  const userMenu = useRootDivCx().userMenu;
 
   return (
     <header className="flex gap-2 p-2 items-center">
       <Sidepanel />
       <div className={`grow ${cn}`}>{children}</div>
-      {userMenuProps ?? userMenuCx}
+      {userMenu}
     </header>
   );
 }
