2078725ab13ea2988df43935b31d67e51054cd0e
Tamás Tóth <tomjtoth@gmail.com>
2025-07-07 14:41:41 +0300

    app/page.tsx to employ RootDiv

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,28 +1,26 @@
+import RootDiv from "@/components/rootDiv";
 import Header from "@/components/header";
-import UserMenu from "@/components/userMenu";
 import HashParagraph from "@/components/hashParagraph";
 import ImportSection from "@/components/import";
 
-export default async function HomePage() {
-  return (
-    <>
-      <Header userMenu={<UserMenu />}>
-        <h2>Saldo</h2>
-      </Header>
+export default () => (
+  <RootDiv>
+    <Header>
+      <h2>Saldo</h2>
+    </Header>
 
-      <div className="p-2">
-        <p>
-          This is a multi-user expense tracker app. Most revisions of records
-          are archived and can be viewed by users related to them, but deletion
-          is not supported only setting an{" "}
-          <span className="p-1 rounded border-2 border-red-500">INACTIVE</span>{" "}
-          status.
-        </p>
+    <div className="p-2">
+      <p>
+        This is a multi-user expense tracker app. Most revisions of records are
+        archived and can be viewed by users related to them, but deletion is not
+        supported only setting an{" "}
+        <span className="p-1 rounded border-2 border-red-500">INACTIVE</span>{" "}
+        status.
+      </p>
 
-        <HashParagraph />
+      <HashParagraph />
 
-        <ImportSection />
-      </div>
-    </>
-  );
-}
+      <ImportSection />
+    </div>
+  </RootDiv>
+);
