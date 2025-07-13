d25c44604f003b907e3ce4106c20a38771337687
Tamás Tóth <tomjtoth@gmail.com>
2025-07-13 11:35:43 +0300

    rebased Items onto ModelSRI

--- src/lib/models/item.ts
+++ src/lib/models/item.ts
@@ -1,102 +1,36 @@
-import { DataTypes, Model, ModelAttributes } from "sequelize";
+import { ModelSRI, TCrModelSRI, TModelSRI } from "./model";
 
-import {
-  seqIdCols,
-  seqInitOpts,
-  REV_ID_INTEGER_PK,
-  TIDs,
-  TCrIDs,
-  Status,
-  Revision,
-} from "./common";
-import { Category } from "./category";
-import { Receipt } from "./receipt";
-import { ItemShare } from "./itemShare";
-
-export type TItem = TIDs & {
-  rcptId: number;
-  catId: number;
+type TItemBase = {
+  receiptId: number;
+  categoryId: number;
   cost: number;
   notes?: string;
-
-  Revision?: Revision;
-  Status?: Status;
-  Receipt?: Receipt;
-  Category?: Category;
-
-  shares?: ItemShare[];
 };
 
-export type TCrItem = TCrIDs &
-  Pick<TItem, "rcptId" | "catId" | "cost" | "notes">;
+export type TItem = TModelSRI & TItemBase;
+export type TCrItem = TCrModelSRI & TItemBase;
 
-/**
- * used in both Xy and XyArchive, but Archive additionally implements revId as PK
- */
-const COLS: ModelAttributes<Item, TItem> = {
-  ...seqIdCols,
-  rcptId: {
-    type: DataTypes.INTEGER,
-    references: { model: Receipt, key: "id" },
-  },
-  catId: {
-    type: DataTypes.INTEGER,
-    references: { model: Category, key: "id" },
-  },
-  cost: {
-    type: DataTypes.INTEGER,
-    allowNull: false,
-
-    get() {
-      return this.getDataValue("cost") / 100;
+export const Items = new ModelSRI<TItem, TCrItem>(
+  "items",
+  {
+    receiptId: {
+      type: "number",
+      required: true,
     },
-
-    set(val: number) {
-      this.setDataValue("cost", Math.round(val * 100));
+    categoryId: {
+      type: "number",
+      required: true,
+    },
+    cost: {
+      type: "number",
+      required: true,
+    },
+    notes: {
+      type: "string",
     },
   },
-  notes: { type: DataTypes.TEXT },
-};
-
-class Common extends Model<TItem, TCrItem> {
-  id!: number;
-  revId!: number;
-  statusId!: number;
-
-  rcptId!: number;
-  catId!: number;
-  cost!: number;
-  notes?: string;
-
-  Revision?: Revision;
-  Status?: Status;
-  Receipt?: Receipt;
-  Category?: Category;
-
-  shares?: ItemShare[];
-}
-
-export class Item extends Common {
-  archives?: ItemArchive[];
-}
-
-Item.init(COLS, {
-  ...seqInitOpts,
-  modelName: "Item",
-});
-
-export class ItemArchive extends Common {
-  current?: Item;
-}
-
-ItemArchive.init(
   {
-    ...COLS,
-    ...REV_ID_INTEGER_PK,
-  },
-  {
-    ...seqInitOpts,
-    modelName: "ItemArchive",
-    tableName: "items_archive",
+    toJS: (row) => ({ ...row, cost: row.cost / 100 }),
+    toDB: (obj) => ({ ...obj, cost: Math.round(obj.cost * 100) }),
   }
 );
