b5ada040191888a5829e60b0975cdf5bdd955dc1
Tamás Tóth <tomjtoth@gmail.com>
2025-07-13 11:37:10 +0300

    rebased Users onto ModelSRI

--- src/lib/models/user.ts
+++ src/lib/models/user.ts
@@ -1,90 +1,34 @@
-import { DataTypes, Model, ModelAttributes } from "sequelize";
+import { has3ConsecutiveLetters, isEmail } from "../utils";
+import { ModelSRI, TCrModelSRI, TModelSRI } from "./model";
 
-import type { TCrIDs, TIDs } from "./common";
-import { seqIdCols, seqInitOpts, REV_ID_INTEGER_PK } from "./common";
-import { has3ConsecutiveLetters } from "../utils";
-import { Membership } from "./membership";
-import { Group } from "./group";
-
-export type TUser = TIDs & {
+type TUserBase = {
   email: string;
   name: string;
   image?: string;
   defaultGroupId?: number;
-
-  Membership?: Membership;
-  Groups?: Group[];
 };
 
-export type TCrUser = TCrIDs & Pick<TUser, "email" | "name">;
+export type TUser = TModelSRI & TUserBase;
+export type TCrUser = TCrModelSRI & TUserBase;
 
-/**
- * used in both Xy and XyArchive, but Archive additionally implements revId as PK
- */
-const COLS: ModelAttributes<User, TUser> = {
-  ...seqIdCols,
-
+export const Users = new ModelSRI<TUser, TCrUser>("users", {
   email: {
-    type: DataTypes.TEXT,
-    allowNull: false,
-    unique: true,
-    validate: {
-      isEmail: true,
-    },
+    type: "string",
+    required: true,
+    validators: [isEmail],
   },
 
   name: {
-    type: DataTypes.TEXT,
-    allowNull: false,
-    validate: {
-      has3ConsecutiveLetters,
-    },
+    type: "string",
+    required: true,
+    validators: [has3ConsecutiveLetters],
   },
 
   image: {
-    type: DataTypes.TEXT,
+    type: "string",
   },
 
   defaultGroupId: {
-    type: DataTypes.INTEGER,
-    references: { model: Group, key: "id" },
+    type: "number",
   },
-};
-
-class Common extends Model<TUser, TCrUser> {
-  id!: number;
-  revId?: number;
-  statusId!: number;
-  email!: string;
-  name!: string;
-  image?: string;
-  defaultGroupId?: number;
-
-  Membership?: Membership;
-  Groups?: Group[];
-}
-
-export class User extends Common {
-  archives?: UserArchive[];
-}
-
-User.init(COLS, {
-  ...seqInitOpts,
-  modelName: "User",
 });
-
-export class UserArchive extends Common {
-  current?: User;
-}
-
-UserArchive.init(
-  {
-    ...COLS,
-    ...REV_ID_INTEGER_PK,
-  },
-  {
-    ...seqInitOpts,
-    modelName: "UserArchive",
-    tableName: "users_archive",
-  }
-);
