1bade2391f8074b600dca991d5533bd9aca55b1a
Tamás Tóth <tomjtoth@gmail.com>
2025-07-13 11:32:36 +0300

    importV3 to use new models

--- src/lib/services/importV3/index.ts
+++ src/lib/services/importV3/index.ts
@@ -1,64 +1,47 @@
 "use server";
 
-import { parseCSV, parseData, TDBData } from "./parsers";
+import { db, migrator } from "@/lib/db";
 import {
-  Revision,
-  User,
-  Category,
-  Receipt,
-  Item,
-  ItemShare,
-  atomic,
-  Group,
-  Membership,
+  Categories,
+  Groups,
+  Items,
+  ItemShares,
+  Memberships,
+  Receipts,
+  Users,
 } from "@/lib/models";
+import { parseCSV, parseData, TDBData } from "./parsers";
+import { Revisions } from "@/lib/models/revision";
 
-export const alreadyInProd = async () => {
-  const user = await User.findByPk(1);
-
-  return !!user && user.email !== "user0@just.imported";
-};
+export async function alreadyInProd() {
+  return !!Users.get(
+    `SELECT 1 FROM users 
+    WHERE id = 1 AND email != 'user0@just.imported'`,
+    []
+  );
+}
 
 export async function importV3() {
   const read = await parseCSV(process.env.CSV_PATH || "data/saldo-v3.csv");
   const parsed = parseData(read);
-  return await insertData(parsed);
+  return insertData(parsed);
 }
 
 export async function insertData(data: TDBData) {
-  return await atomic("importing from V3", async (transaction) => {
-    // drop ALL imported data
-    await Revision.truncate({ transaction, cascade: true });
+  return db
+    .transaction(() => {
+      migrator.truncate();
 
-    // these 2 can be run in parallel
-    const [revisions, users] = await Promise.all([
-      Revision.bulkCreate(data.revisions, { transaction }),
-      User.bulkCreate(data.users, { transaction }),
-    ]);
-
-    // the rest are to be inserted in strict order
-    const groups = await Group.bulkCreate(data.groups, { transaction });
-    const memberships = await Membership.bulkCreate(data.memberships, {
-      transaction,
-    });
-    const categories = await Category.bulkCreate(data.categories, {
-      transaction,
-    });
-    const receipts = await Receipt.bulkCreate(data.receipts, { transaction });
-    const items = await Item.bulkCreate(data.items, { transaction });
-    const itemShares = await ItemShare.bulkCreate(data.itemShares, {
-      transaction,
-    });
-
-    return {
-      revisions: revisions.length,
-      users: users.length,
-      groups: groups.length,
-      memberships: memberships.length,
-      categories: categories.length,
-      receipts: receipts.length,
-      items: items.length,
-      itemShares: itemShares.length,
-    };
-  });
+      return {
+        revisions: Revisions.insert(data.revisions).length,
+        users: Users.insert(data.users).length,
+        groups: Groups.insert(data.groups).length,
+        memberships: Memberships.insert(data.memberships).length,
+        categories: Categories.insert(data.categories).length,
+        receipts: Receipts.insert(data.receipts).length,
+        items: Items.insert(data.items).length,
+        itemShares: ItemShares.insert(data.itemShares).length,
+      };
+    })
+    .deferred();
 }
