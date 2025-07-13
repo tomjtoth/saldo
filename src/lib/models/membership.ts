e46861ef0a5cf2546094a195ff5a6a10437ff252
Tamás Tóth <tomjtoth@gmail.com>
2025-07-13 11:36:07 +0300

    rebased Memberships onto ModelSR

--- src/lib/models/membership.ts
+++ src/lib/models/membership.ts
@@ -1,73 +1,31 @@
-import { DataTypes, Model, ModelAttributes } from "sequelize";
+import { ModelSR, TCrModelSR, TModelSR } from "./model";
 
-import { REV_ID_INTEGER_PK, seqIdCols, seqInitOpts } from "./common";
-import { Category } from "./category";
-
-/**
- * used in both Xy and XyArchive, but Archive additionally implements revId as PK
- */
-const COLS: ModelAttributes<Membership, TMembership> = {
-  groupId: { type: DataTypes.INTEGER, primaryKey: true },
-  userId: { type: DataTypes.INTEGER, primaryKey: true },
-  revId: seqIdCols.revId,
-  statusId: seqIdCols.statusId,
-  defaultCatId: {
-    type: DataTypes.INTEGER,
-    references: { model: Category, key: "id" },
-  },
-
-  admin: {
-    type: DataTypes.BOOLEAN,
-    defaultValue: false,
-  },
-};
-
-export type TMembership = {
+type TMembershipBase = {
   groupId: number;
   userId: number;
-  revId: number;
-  statusId: number;
-  defaultCatId?: number;
-
+  defaultCategoryId?: number;
   admin: boolean;
-
-  current?: Membership;
-  archives?: MembershipArchive[];
 };
 
-type TCrMembership = Pick<TMembership, "groupId" | "userId" | "revId"> &
-  Partial<Pick<TMembership, "admin" | "statusId">>;
+export type TMembership = TModelSR & TMembershipBase;
+export type TCrMembership = TCrModelSR & TMembershipBase;
 
-class Common extends Model<TMembership, TCrMembership> {
-  groupId!: number;
-  userId!: number;
-  revId!: number;
-  statusId!: number;
-  defaultCatId?: number;
-
-  admin!: boolean;
-
-  current?: Membership;
-  archives?: MembershipArchive[];
-}
-
-export class Membership extends Common {
-  archives?: MembershipArchive[];
-}
-Membership.init(COLS, {
-  ...seqInitOpts,
-  modelName: "Membership",
-});
-
-export class MembershipArchive extends Common {}
-MembershipArchive.init(
+export const Memberships = new ModelSR<TMembership, TCrMembership>(
+  "memberships",
   {
-    ...COLS,
-    ...REV_ID_INTEGER_PK,
-  },
-  {
-    ...seqInitOpts,
-    modelName: "MembershipArchive",
-    tableName: "memberships_archive",
+    groupId: {
+      type: "number",
+      primaryKey: true,
+    },
+    userId: {
+      type: "number",
+      primaryKey: true,
+    },
+    defaultCategoryId: {
+      type: "number",
+    },
+    admin: {
+      type: "boolean",
+    },
   }
 );
