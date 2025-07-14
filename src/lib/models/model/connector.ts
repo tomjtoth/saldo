d9b90ea294aaa8f2b98382182cd14476ea33980a
Tamás Tóth <tomjtoth@gmail.com>
2025-07-14 22:39:48 +0300

    tweakes class Connector, still WiP

--- src/lib/models/model/connector.ts
+++ src/lib/models/model/connector.ts
@@ -1,68 +1,126 @@
+import pluralize from "pluralize";
+
 import { err } from "@/lib/utils";
+import { NumericKeys } from "./types";
 import { Inserter } from "./inserter";
 import { Model } from "./model";
-import { NumericKeys } from "./types";
 import { ModelSR } from "./modelSR";
 import { ModelSRI } from "./modelSRI";
 
-type TJoin =
-  | { table: string; key: string }
-  | Model<any, any>
-  | ModelSR<any, any>
-  | ModelSRI<any, any>;
+const CONNECTION_TABLE: {
+  [fromTable: string]: {
+    [toTable: string]:
+      | {
+          fromId: string;
+          toId: string;
+        }
+      | { through: string };
+  };
+} = {};
+
+let tempKey: string | undefined;
+
+function connect(...params: string[]): void {
+  const [tableA, tableB, ...rest] = params;
+
+  const route =
+    rest.length > 1 ? { fromId: rest[0], toId: rest[1] } : { through: rest[0] };
+
+  if (CONNECTION_TABLE[tableA] === undefined) {
+    CONNECTION_TABLE[tableA] = { [tableB]: route };
+    console.info(`RELATION: ${tableA}.${rest[0]} => ${tableB}.${rest[1]}`);
+  } else {
+    CONNECTION_TABLE[tableA][tableB] = route;
+    console.info(`RELATION: ${tableA} -> ${tableB} via ${rest[0]}`);
+  }
+}
+
+type TAnyModel = Model<any, any> | ModelSR<any, any> | ModelSRI<any, any>;
+type TConnectionHelper = { table: string; keys?: string[]; through?: string };
+type TJoin = TAnyModel | TConnectionHelper;
 
 export class Connector<M, C, D> extends Inserter<M, C, D> {
-  protected tmpKey?: string;
-  protected connections: {
-    [tableName: string]: { myKey: string; key: string };
-  } = {};
+  protected get iterColNames() {
+    return this.iterCols.map(([col]) => col as string);
+  }
 
-  get pkAndTableName() {
-    const len = this.primaryKeys.length;
-    if (len > 1)
-      err(
-        `${this.tableName} has ${len} primaryKeys, use its ".via(key)" method to pick one`
-      );
-
-    return { table: this.tableName, key: this.primaryKeys[0] as string };
+  get keysAndTable() {
+    return {
+      table: this.tableName,
+      keys: this.primaryKeys.length > 1 ? this.iterColNames : this.primaryKeys,
+    } as TConnectionHelper;
   }
 
   column(key: NumericKeys<M>) {
-    this.tmpKey = key as string;
+    tempKey = key as string;
     return this;
   }
 
+  /**
+   * `.have(...)` `.joinTo(...)` `.joinsTo(...)` are synonyms
+   * used during establishing relations between models
+   */
   have(other: TJoin) {
     return this.joinsTo(other);
   }
 
+  /**
+   * `.have(...)` `.joinTo(...)` `.joinsTo(...)` are synonyms
+   * used during establishing relations between models
+   */
   joinTo(other: TJoin) {
     return this.joinsTo(other);
   }
 
+  /**
+   * `.have(...)` `.joinTo(...)` `.joinsTo(...)` are synonyms
+   * used during establishing relations between models
+   */
   joinsTo(other: TJoin) {
-    const { table, key } =
-      other instanceof Model ? other.pkAndTableName : other;
+    const {
+      table: otherTableName,
+      keys,
+      through,
+    } = other instanceof Model ? other.keysAndTable : other;
 
-    const len = this.primaryKeys.length;
-    if (!this.tmpKey && len > 1)
-      err(
-        `${this.tableName} has ${len} primaryKeys, use its ".column(key)" method to pick one`
-      );
+    if (keys) {
+      const thisSingularId = pluralize.singular(this.tableName) + "Id";
+      // const otherSingularId = pluralize.singular(otherTableName) + "Id";
 
-    this.connections[table] = {
-      myKey: this.tmpKey ?? (this.primaryKeys[0] as string),
-      key,
-    };
-    this.tmpKey = undefined;
+      const fromId =
+        tempKey ??
+        // this.iterColNames.find((col) => col === otherSingularId) ??
+        (this.primaryKeys[0] as string);
+
+      const toId =
+        keys.length > 1
+          ? keys.find((key) => key === thisSingularId)
+          : keys.at(0);
+
+      if (!toId)
+        err(
+          `Could not resolve relation between ${
+            this.tableName
+          }.${fromId} and ${otherTableName}.${toId ?? "???"}`
+        );
+
+      connect(this.tableName, otherTableName, fromId, toId);
+    } else if (through) connect(this.tableName, otherTableName, through);
+
+    tempKey = undefined;
   }
 
   via(key: NumericKeys<M>) {
-    return { table: this.tableName, key: key as string };
+    return {
+      table: this.tableName,
+      keys: [key as string],
+    } as TConnectionHelper;
   }
 
-  // TODO
-  through(other: TJoin) {
-    return { table: "", key: "" };
+  through(other: TAnyModel) {
+    return {
+      table: this.tableName,
+      through: other.keysAndTable.table,
+    } as TConnectionHelper;
   }
 }
