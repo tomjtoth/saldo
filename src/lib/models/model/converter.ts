38f33110fff365588cbc68bb623723055d5714b5
Tamás Tóth <tomjtoth@gmail.com>
2025-07-14 17:55:16 +0300

    Converter logic reworked, saved 1 loop, using this.iterCols

--- src/lib/models/model/converter.ts
+++ src/lib/models/model/converter.ts
@@ -1,19 +1,26 @@
 import { asArray, TMix } from "@/lib/utils";
 import { Validator } from "./validator";
+import { TDbValids, TValids } from "./types";
 
-export class Converter<M, C, D> extends Validator<M, C, D> {
+export class Converter<M, C, D> extends Validator<M, C> {
   protected toDB(fromJS: TMix<M>) {
-    const arr = asArray(fromJS) as M[] | D[];
+    const arr = asArray(fromJS);
 
     return arr.map((row) => {
-      let tmp = row as D | M;
+      let tmp = row as M | D;
 
-      if (this._toDB) tmp = this._toDB(tmp as M);
+      this.iterCols.forEach(([col, { toDB, type }]) => {
+        const keyMD = col as keyof (M | D);
 
-      this.boolCols.forEach(([key]) => {
-        (tmp[key as keyof (D | M)] as boolean) = (tmp[key as keyof (D | M)]
-          ? 1
-          : 0) as unknown as boolean;
+        if (tmp[keyMD] !== undefined) {
+          // columns specific conversion first
+          if (toDB) (tmp[keyMD] as TDbValids) = toDB(tmp[keyMD] as TValids);
+
+          // then general
+          if (type === "boolean") {
+            (tmp[keyMD] as number) = (tmp[keyMD] ? 1 : 0) as number;
+          }
+        }
       });
 
       return tmp as D;
@@ -21,17 +28,25 @@ export class Converter<M, C, D> extends Validator<M, C, D> {
   }
 
   protected toJS(fromDB: TMix<D>) {
-    const arr = asArray(fromDB) as D[] | M[];
+    const arr = asArray(fromDB);
 
     return arr.map((row) => {
-      let tmp = row as D | M;
+      let tmp = row as M | D;
 
-      if (this._toJS) tmp = this._toJS(tmp as D);
+      this.iterCols.forEach(([col, { toJS, type }]) => {
+        const keyMD = col as keyof (M | D);
 
-      this.boolCols.forEach(([key]) => {
-        const asInt = (tmp[key as keyof (D | M)] ? 1 : 0) as unknown as boolean;
+        if (tmp[keyMD] !== undefined) {
+          // general conversion first
+          if (type === "boolean") {
+            const asInt = (tmp[keyMD] ? 1 : 0) as unknown as boolean;
 
-        (tmp[key as keyof (D | M)] as boolean) = asInt;
+            (tmp[keyMD] as boolean) = asInt;
+          }
+
+          // then column specific
+          if (toJS) (tmp[keyMD] as TValids) = toJS(tmp[keyMD] as TDbValids);
+        }
       });
 
       return tmp as M;
