4dcdcdd4cd7cd95d0582d53776fd271b3d059873
Tamás Tóth <tomjtoth@gmail.com>
2025-07-14 06:51:52 +0300

    added connections between models

--- src/lib/models/index.ts
+++ src/lib/models/index.ts
@@ -1,3 +1,12 @@
+import { Categories } from "./category";
+import { Groups } from "./group";
+import { Items } from "./item";
+import { ItemShares } from "./itemShare";
+import { Memberships } from "./membership";
+import { Receipts } from "./receipt";
+import { Revisions } from "./revision";
+import { Users } from "./user";
+
 export * from "./revision";
 export * from "./user";
 export * from "./group";
@@ -6,3 +15,20 @@ export * from "./category";
 export * from "./receipt";
 export * from "./item";
 export * from "./itemShare";
+
+Revisions.column("revisedBy").joinsTo(Users);
+
+Users.have(Revisions.via("revisedBy"));
+Users.column("revisionId").joinsTo(Revisions);
+Users.have(Memberships.via("userId"));
+
+Groups.have(Categories.via("groupId"));
+Groups.have(Memberships.via("groupId"));
+Groups.have(Receipts.via("groupId"));
+
+Groups.have(Users.through(Memberships));
+
+Categories.joinTo(Groups);
+Items.joinTo(ItemShares.via("itemId"));
+
+Users.joinTo(Groups.through(Memberships));
