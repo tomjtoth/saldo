103d5dc9a829b1a1f7a3656168a6bc8bc5cc8020
Tamás Tóth <tomjtoth@gmail.com>
2025-07-20 21:07:06 +0300

    pulled class Core related types to model/core.ts

--- src/lib/models/model/core.ts
+++ src/lib/models/model/core.ts
@@ -1,4 +1,38 @@
-import { TModelColumn } from "./types";
+import { TDbValids, TValids } from "./types";
+
+type TColBase = {
+  /**
+   * defaults to false
+   */
+  required?: true;
+
+  /**
+   * setting multiple columns to true results in a compound key
+   */
+  primaryKey?: true;
+
+  /**
+   * when updating only this oclumn of a row, archival will be skipped
+   */
+  skipArchival?: true;
+  validators?: ((val: unknown) => void)[];
+
+  toJS?: (fromDB: TDbValids) => TValids;
+  toDB?: (fromJS: TValids) => TDbValids;
+};
+
+type TypeMap = {
+  string: string;
+  number: number;
+  boolean: boolean;
+};
+
+type TModelColumn = {
+  [K in keyof TypeMap]: TColBase & {
+    type: K;
+    defaultValue?: (() => TypeMap[K]) | TypeMap[K];
+  };
+}[keyof TypeMap];
 
 export class Core<M> {
   protected tableName;
