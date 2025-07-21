d420a5a691d88a42305852acd9e5a0bc79cb4b3c
Tamás Tóth <tomjtoth@gmail.com>
2025-07-21 17:28:18 +0300

    model/types.ts to accommodate the operators and queryBuilder related

--- src/lib/models/model/types.ts
+++ src/lib/models/model/types.ts
@@ -1,42 +1,123 @@
-type TValidatorFn = (val: unknown) => void;
+import { QueryWrapper } from "./queryWrapper";
+import { QueryBuilder } from "./queryBuilder";
 
 export type TDbValids = number | string | null;
 export type TValids = TDbValids | boolean | object;
 
-type TColBase = {
+export type TSelectKeys<T> = {
+  [P in keyof T]: T[P] extends TDbValids ? P : never;
+}[keyof T];
+
+type TOpOr<T> = {
   /**
-   * defaults to false
+   * An array of alternatives
+   */
+  $OR: TWhere<T>[];
+};
+
+type TOpNot<T> = { $NOT: T };
+
+type TLiteral = {
+  /**
+   * injects the provided string into the statement
+   */
+  $SQL: string;
+};
+
+type TOpLt<T> = {
+  /**
+   * Less Than
+   */
+  $LT: T;
+};
+
+type TOpLe<T> = {
+  /**
+   * Less than or Equal to
+   */
+  $LE: T;
+};
+
+type TOpGt<T> = {
+  /**
+   * Greater Than
+   */
+  $GT: T | TLiteral;
+};
+
+type TOpGe<T> = {
+  /**
+   * Greater than or Equal to
+   */
+  $GE: T;
+};
+
+type TOpBetween<T> = { $BETWEEN: [T, T] };
+type TOpIn<T> = { $IN: T[] };
+type TOpLike = { $LIKE: string };
+
+type TStrOps =
+  | null
+  | string
+  | TLiteral
+  | TOpIn<string>
+  | TOpBetween<string>
+  | TOpLt<string>
+  | TOpLe<string>
+  | TOpGt<string>
+  | TOpGe<string>
+  | TOpLike;
+
+type TNumOps =
+  | null
+  | number
+  | TLiteral
+  | TOpIn<number>
+  | TOpBetween<number>
+  | TOpLt<number>
+  | TOpLe<number>
+  | TOpGt<number>
+  | TOpGe<number>;
+
+export type TWhere<D> =
+  | TOpOr<D>
+  | {
+      [P in keyof D]?: D[P] extends number
+        ? TNumOps | TOpNot<TNumOps>
+        : D[P] extends string
+        ? TStrOps | TOpNot<TStrOps>
+        : never;
+    };
+
+export type TQuery<D> = {
+  select?: (keyof D)[];
+  join?: TQuery<D>[];
+  table?: string;
+
+  /**
+   * turns the `LEFT JOIN` into an `INNER JOIN`
    */
   required?: true;
 
   /**
-   * setting multiple columns to true results in a compound key
+   * all members of this level are connected by "AND"
    */
-  primaryKey?: true;
-
-  /**
-   * when updating only this oclumn of a row, archival will be skipped
-   */
-  skipArchival?: true;
-  validators?: TValidatorFn[];
-
-  toJS?: (fromDB: TDbValids) => TValids;
-  toDB?: (fromJS: TValids) => TDbValids;
+  where?: TWhere<D>;
 };
 
-type TypeMap = {
-  string: string;
-  number: number;
-  boolean: boolean;
+export type TOther<M = any, D = any> =
+  | QueryWrapper<M, any, D>
+  | QueryBuilder<M, D>;
+
+export type TModelSR = {
+  revisionId: number;
+  statusId: number;
 };
 
-export type TModelColumn = {
-  [K in keyof TypeMap]: TColBase & {
-    type: K;
-    defaultValue?: (() => TypeMap[K]) | TypeMap[K];
-  };
-}[keyof TypeMap];
+export type TCrModelSR = Partial<TModelSR>;
 
-export type NumericKeys<T> = {
-  [K in keyof T]: T[K] extends number ? K : never;
-}[keyof T];
+export type TModelSRI = TModelSR & {
+  id: number;
+};
+
+export type TCrModelSRI = TCrModelSR & { id?: number };
