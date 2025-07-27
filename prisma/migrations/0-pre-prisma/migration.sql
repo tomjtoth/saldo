0a58fcb5d1eca532f5d7c361148592eb413b6a5f
Tamás Tóth <tomjtoth@gmail.com>
2025-07-27 15:57:52 +0300

    added the consumption view to 0-pre-prisma

--- prisma/migrations/0-pre-prisma/migration.sql
+++ prisma/migrations/0-pre-prisma/migration.sql
@@ -219,3 +219,17 @@ Pragma writable_schema=1;
 CREATE UNIQUE INDEX "sqlite_autoindex_users_1" ON "users"("email");
 Pragma writable_schema=0;
 
+CREATE VIEW consumption AS
+SELECT
+  r.group_id AS groupId,
+  r.paid_on AS paidOn,
+  r.paid_by AS paidBy,
+  COALESCE(sh.user_id, r.paid_by) AS paidTo,
+  i.id AS itemId,
+  i.cat_id AS catId,
+  cost / 100.0 * COALESCE(share * 1.0 / SUM(share) OVER (PARTITION BY i.id), 1) AS share
+FROM receipts r
+INNER JOIN items i ON r.id = i.rcpt_id
+LEFT JOIN item_shares sh ON (sh.item_id = i.id AND sh.status_id = 1)
+WHERE r.status_id = 1 AND i.status_id = 1
+ORDER BY paid_on;
