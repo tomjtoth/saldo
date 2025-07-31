f812782910bfb68378ecc9ef75489a1fbffdc16b
Tamás Tóth <tomjtoth@gmail.com>
2025-07-31 18:19:42 +0300

    synced Prisma migrations, adjusted column names

--- prisma/migrations/20250731143538_baseline/migration.sql
+++ prisma/migrations/20250731143538_baseline/migration.sql
@@ -2,34 +2,38 @@ PRAGMA foreign_keys = OFF;
 BEGIN DEFERRED TRANSACTION;
 
 
--- create new schema
+
+-------------------
+-- create schema --
+-------------------
 
 
-CREATE TABLE "Meta" (
-    "id" INTEGER NOT NULL PRIMARY KEY,
+
+CREATE TABLE IF NOT EXISTS "Meta" (
+    "id" INTEGER PRIMARY KEY,
     "info" TEXT NOT NULL,
-    "data" JSONB NOT NULL
+    "data" BLOB
 );
 
 CREATE TABLE "Archive" (
-    "id" INTEGER NOT NULL PRIMARY KEY,
-    "tableId" INTEGER NOT NULL,
+    "id" INTEGER PRIMARY KEY,
+    "tableColumnId" INTEGER NOT NULL,
     "entityPk1" INTEGER NOT NULL,
     "entityPk2" INTEGER,
     "revisionId" INTEGER NOT NULL,
-    "payload" JSONB NOT NULL,
+    "data" BLOB,
     CONSTRAINT "Archive_revisionId_fkey" FOREIGN KEY ("revisionId") REFERENCES "Revision" ("id") ON DELETE CASCADE ON UPDATE CASCADE
 );
 
 CREATE TABLE "Revision" (
-    "id" INTEGER NOT NULL PRIMARY KEY,
-    "createdOn" INTEGER NOT NULL,
-    "createdById" INTEGER NOT NULL,
-    CONSTRAINT "Revision_createdById_fkey" FOREIGN KEY ("createdById") REFERENCES "User" ("id") ON UPDATE CASCADE
+    "id" INTEGER PRIMARY KEY,
+    "createdAt" INTEGER NOT NULL,
+    "createdBy" INTEGER NOT NULL,
+    CONSTRAINT "Revision_createdBy_fkey" FOREIGN KEY ("createdBy") REFERENCES "User" ("id") ON UPDATE CASCADE
 );
 
 CREATE TABLE "User" (
-    "id" INTEGER NOT NULL PRIMARY KEY,
+    "id" INTEGER PRIMARY KEY,
     "revisionId" INTEGER NOT NULL,
     "statusId" INTEGER NOT NULL DEFAULT 0,
     "email" TEXT NOT NULL,
@@ -41,7 +45,7 @@ CREATE TABLE "User" (
 );
 
 CREATE TABLE "Group" (
-    "id" INTEGER NOT NULL PRIMARY KEY,
+    "id" INTEGER PRIMARY KEY,
     "revisionId" INTEGER NOT NULL,
     "statusId" INTEGER NOT NULL DEFAULT 0,
     "name" TEXT NOT NULL,
@@ -65,7 +69,7 @@ CREATE TABLE "Membership" (
 );
 
 CREATE TABLE "Category" (
-    "id" INTEGER NOT NULL PRIMARY KEY,
+    "id" INTEGER PRIMARY KEY,
     "revisionId" INTEGER NOT NULL,
     "statusId" INTEGER NOT NULL DEFAULT 0,
     "groupId" INTEGER NOT NULL,
@@ -76,19 +80,19 @@ CREATE TABLE "Category" (
 );
 
 CREATE TABLE "Receipt" (
-    "id" INTEGER NOT NULL PRIMARY KEY,
+    "id" INTEGER PRIMARY KEY,
     "revisionId" INTEGER NOT NULL,
     "statusId" INTEGER NOT NULL DEFAULT 0,
     "groupId" INTEGER NOT NULL,
-    "paidById" INTEGER NOT NULL,
+    "paidBy" INTEGER NOT NULL,
     "paidOn" INTEGER NOT NULL,
     CONSTRAINT "Receipt_revisionId_fkey" FOREIGN KEY ("revisionId") REFERENCES "Revision" ("id") ON DELETE CASCADE ON UPDATE CASCADE,
     CONSTRAINT "Receipt_groupId_fkey" FOREIGN KEY ("groupId") REFERENCES "Group" ("id") ON UPDATE CASCADE,
-    CONSTRAINT "Receipt_paidById_fkey" FOREIGN KEY ("paidById") REFERENCES "User" ("id") ON UPDATE CASCADE
+    CONSTRAINT "Receipt_paidBy_fkey" FOREIGN KEY ("paidBy") REFERENCES "User" ("id") ON UPDATE CASCADE
 );
 
 CREATE TABLE "Item" (
-    "id" INTEGER NOT NULL PRIMARY KEY,
+    "id" INTEGER PRIMARY KEY,
     "revisionId" INTEGER NOT NULL,
     "statusId" INTEGER NOT NULL DEFAULT 0,
     "receiptId" INTEGER NOT NULL,
@@ -113,87 +117,97 @@ CREATE TABLE "ItemShare" (
     CONSTRAINT "ItemShare_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User" ("id") ON UPDATE CASCADE
 );
 
-CREATE INDEX "Archive_tableId_entityPk1_entityPk2_idx" ON "Archive"("tableId", "entityPk1", "entityPk2");
+CREATE UNIQUE INDEX IF NOT EXISTS "Meta_info_key" ON "Meta"("info");
 
 
--- transfer data from old schema to new
+
+-------------------
+-- transfer data --
+-------------------
 
 
-INSERT INTO Meta (info, data)
-    SELECT 'datetime', jsonb_object('anchor', '2020-01-01', 'timezone', 'Europe/Helsinki')
+
+INSERT INTO "Meta" ("info", "data")
+    SELECT 'datetime', json_object('anchor', '2020-01-01', 'timezone', 'Europe/Helsinki')
     UNION
-    SELECT 'statuses',  jsonb_array('ACTIVE', 'INACTIVE')
+    SELECT 'statuses',  json_array('ACTIVE', 'INACTIVE')
     UNION
-    SELECT 'tableNames', jsonb_array('Group', 'Category', 'Membership');
+    SELECT 'tableColumnId', json_array(
+        'categories.name', 'categories.description', 'categories.status_id', 'categories.group_id',
+        'groups.name', 'groups.description', 'groups.status_id', 'groups.uuid',
+        'memberships.status_id', 'memberships.default_category_id'
+    );
 
-INSERT INTO "Revision" (id, createdOn, createdById)
-    SELECT id, rev_on, rev_by FROM REVISIONS;
+INSERT INTO Archive (entityPk1, entityPk2, revisionId, tableColumnId, data)
+    SELECT id, NULL, rev_id, 2, status_id - 1 FROM categories_archive
+    UNION
+    SELECT id, NULL, rev_id, 3, group_id FROM categories_archive
+    UNION
+    SELECT id, NULL, rev_id, 0, name FROM categories_archive
+    UNION
+    SELECT id, NULL, rev_id, 1, description FROM categories_archive
+    UNION
+    SELECT id, NULL, rev_id, 6, status_id - 1 FROM groups_archive
+    UNION
+    SELECT id, NULL, rev_id, 4, name FROM groups_archive
+    UNION
+    SELECT id, NULL, rev_id, 5, description FROM groups_archive
+    UNION
+    SELECT id, NULL, rev_id, 7, uuid FROM groups_archive
+    UNION
+    SELECT group_id, user_id, rev_id, 8, status_id - 1 + 2 * admin FROM memberships_archive
+    UNION
+    SELECT group_id, user_id, rev_id, 9, default_cat_id FROM memberships_archive;
+
+
+INSERT INTO "Revision" (id, createdAt, createdBy)
+    SELECT id, rev_on, rev_by FROM revisions;
 
 INSERT INTO "User"
     (id, revisionId, statusId, email, name, defaultGroupId)
     SELECT id, rev_id, status_id, email, name, default_group_id
-    FROM USERS;
+    FROM users;
 
 INSERT INTO "Group"
     (id, revisionId, statusId, name, description, uuid)
     SELECT id, rev_id, status_id, name, description, uuid
-    FROM GROUPS;
-
-INSERT INTO "Archive" (tableId, entityPk1, revisionId, payload)
-    SELECT 0, id, rev_id, jsonb_object(
-        'statusId', status_id, 
-        'name', name, 
-        'description', description, 
-        'uuid', uuid
-    )
-    FROM groups_archive;
+    FROM groups;
 
 INSERT INTO "Membership" 
     (groupId, userId, revisionId, statusId, defaultCategoryId)
     SELECT group_id, user_id, rev_id, status_id + 2 * admin, default_cat_id
-    FROM MEMBERSHIPS;
+    FROM memberships;
 
-INSERT INTO "Archive" (tableId, entityPk1, entityPk2, revisionId, payload)
-    SELECT 2, group_id, user_id, rev_id, jsonb_object(
-        'statusId', status_id + 2* admin,
-        'defaultCategoryId', default_cat_id
-    )
-    FROM memberships_archive;
 
 INSERT INTO "Category" 
     (id, revisionId, statusId, groupId, name, description)
     SELECT id, rev_id, status_id, group_id, name, description
-    FROM CATEGORIES;
-    
-INSERT INTO "Archive" (tableId, entityPk1, revisionId, payload)
-    SELECT 1, id, rev_id, jsonb_object(
-        'status_id', status_id,
-        'group_id', group_id,
-        'name', name,
-        'description', description
-    )
-    FROM categories_archive;
+    FROM categories;
 
 INSERT INTO "Receipt" 
-    (id, revisionId, statusId, groupId, paidOn, paidById)
+    (id, revisionId, statusId, groupId, paidOn, paidBy)
     SELECT id, rev_id, status_id, group_id, paid_on, paid_by
-    FROM RECEIPTS;
+    FROM receipts;
 
 INSERT INTO "Item" 
     (id, revisionId, statusId, receiptId, categoryId, cost, notes)
     SELECT id, rev_id, status_id, rcpt_id, cat_id, cost, notes
-    FROM ITEMS;
+    FROM items;
 
 INSERT INTO "ItemShare" 
     (itemId, userId, revisionId, statusId, share)
     SELECT item_id, user_id, rev_id, status_id, share
-    FROM ITEM_SHARES;
+    FROM item_shares;
 
 
--- delete old tables
+
+-----------------------
+-- delete old tables --
+-----------------------
 
 
-DROP TABLE "migrations";
+
+DROP TABLE IF EXISTS "migrations";
 
 DROP TABLE "categories";
 DROP TABLE "categories_archive";
@@ -219,7 +233,7 @@ DROP TABLE "statuses";
 DROP TABLE "users";
 DROP TABLE "users_archive";
 
-DROP VIEW consumption;
+DROP VIEW "consumption";
 
 
 COMMIT TRANSACTION;
