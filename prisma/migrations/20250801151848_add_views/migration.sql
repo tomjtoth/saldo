a86ce7cbe2b073420ae5d07ea25cd98d91c1ce8c
Tamás Tóth <tomjtoth@gmail.com>
2025-08-11 09:41:36 +0300

    updated 3 views

--- prisma/migrations/20250801151848_add_views/migration.sql
+++ prisma/migrations/20250801151848_add_views/migration.sql
@@ -15,24 +15,52 @@ ORDER BY paidOn;
 
 CREATE VIEW "tableColumnNames" AS
 SELECT
-    key AS id,
-    substr(value, 0,instr(value, '.')) AS tableName,
-    substr(value, instr(value, '.')+1, length(value)) AS columnName
-FROM "Meta", json_each(data) 
-WHERE info = 'tableColumnId';
+    cast(key AS INTEGER) AS id,
+    cast(substr(value, 0,instr(value, '.')) AS  TEXT) AS tableName,
+    cast(substr(value, instr(value, '.')+1, length(value)) AS TEXT) AS columnName
+FROM "Meta", json_each(data)
+WHERE info = 'tableColumnIds';
 
 CREATE VIEW "archives" AS
+WITH by_changes AS (
+    SELECT
+        tableName,
+        entityPk1 AS pk1,
+        entityPk2 AS pk2,
+        json_insert(
+            json_group_object("names".columnName, data),
+            '$.revision', json_object(
+                'createdAtInt', r.createdAt,
+                'createdBy', json_object('name', u.name )
+            ),
+            '$.revisionId', a.revisionId
+        ) AS "data"
+    FROM "Archive" a
+    INNER JOIN "tableColumnNames" AS "names" ON (a.tableColumnId = "names".id)
+    INNER JOIN "Revision" r ON r.id = a.revisionId
+    INNER JOIN "User" u ON u.id = r.createdBy
+    GROUP BY tableName, entityPk1, entityPk2, a.revisionId
+    ORDER BY r.createdAt DESC
+),
+
+by_pk2 AS (
+    SELECT
+        tableName,
+        pk1,
+        concat( json_quote(cast(coalesce(pk2, 'null') AS TEXT)), ': [', group_concat(data), ']') AS data
+    FROM by_changes
+    GROUP BY tableName, pk1, pk2
+),
+
+by_pk1 AS (
+    SELECT
+        tableName,
+        concat( json_quote(cast(pk1 AS TEXT)), ': {' , group_concat(data), '}') AS data
+    FROM by_pk2
+    GROUP BY tableName, pk1
+)
+
 SELECT
     tableName,
-    entityPk1 AS pk1,
-    entityPk2 AS pk2,
-	a.revisionId AS revisionId,
-    json_group_object("names".columnName, data) AS payload,
-    r.createdAt AS "revision.createdAtInt",
-	u.name AS "revision.createdBy.name"
-FROM "Archive" a
-INNER JOIN "tableColumnNames" AS "names" ON (a.tableColumnId = "names".id)
-INNER JOIN "Revision" r ON r.id = a.revisionId
-INNER JOIN "User" u ON u.id = r.createdBy
-GROUP BY tableName, entityPk1, entityPk2, a.revisionId
-ORDER BY a.revisionId DESC;
+    concat('{', group_concat(data), '}') AS data
+FROM by_pk1 GROUP BY tableName
