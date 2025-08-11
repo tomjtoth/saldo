b6afe6cb63fc9f226c84ac84f9b11a3ee8fd06a1
Tamás Tóth <tomjtoth@gmail.com>
2025-08-11 13:56:49 +0300

    adjusted receipts service to Prisma

--- src/lib/services/receipts.ts
+++ src/lib/services/receipts.ts
@@ -1,120 +1,122 @@
-import { col, fn, IncludeOptions, Op } from "sequelize";
-
-import {
-  atomic,
-  Category,
-  Group,
-  Item,
-  ItemShare,
-  Membership,
-  Receipt,
-  ReceiptArchive,
-  Revision,
-  TCrItemShare,
-  User,
-} from "../models";
+import { atomic, db, getArchivePopulator, TGroup, TReceipt } from "@/lib/db";
 import { TCliReceipt } from "../reducers";
+import { dateToInt, sortByName } from "../utils";
 
 export type TReceiptInput = TCliReceipt & { groupId: number };
 
-const RCPT_INCLUDE = {
-  include: [
-    {
-      model: Revision,
-      attributes: ["revOn"],
-      include: [{ model: User, attributes: ["name"] }],
-    },
-    {
-      model: ReceiptArchive,
-      as: "archives",
-      include: [
-        {
-          model: Revision,
-          attributes: ["revOn"],
-          include: [{ model: User, attributes: ["name"] }],
-        },
-      ],
-    },
-    Item,
-    { model: User, attributes: ["name"] },
-  ],
-};
-
 export async function addReceipt(addedBy: number, data: TReceiptInput) {
-  return await atomic("Adding receipt", async (transaction) => {
-    const rev = await Revision.create({ revBy: addedBy }, { transaction });
+  return await atomic(
+    { operation: "Adding receipt", revisedBy: addedBy },
+    async (tx, rev) => {
+      const rcpt = await tx.receipt.create({
+        select: {
+          id: true,
+          groupId: true,
+          revision: {
+            select: {
+              createdAtInt: true,
+              createdBy: { select: { name: true } },
+            },
+          },
+          items: { include: { itemShares: true } },
+          paidOnInt: true,
+          paidBy: { select: { name: true } },
+        },
+        data: {
+          groupId: data.groupId,
+          revisionId: rev.id!,
+          paidOnInt: dateToInt(data.paidOn),
+          paidById: data.paidBy,
+          items: {
+            createMany: {
+              data: data.items.map((i) => ({
+                revisionId: rev.id!,
+                categoryId: i.catId,
 
-    const rcpt = await Receipt.create(
-      {
-        groupId: data.groupId,
-        revId: rev.id,
-        paidOn: data.paidOn,
-        paidBy: data.paidBy,
-      },
-      { transaction }
-    );
+                cost: Math.round(parseFloat(i.cost) * 100),
 
-    const items = await Item.bulkCreate(
-      data.items.map((i) => ({
-        revId: rev.id,
-        rcptId: rcpt.id,
-        catId: i.catId,
-        cost: parseFloat(i.cost),
-        notes: i.notes == "" ? undefined : i.notes,
-      })),
-      { transaction }
-    );
-
-    // this relies on Sequelize returning the items in exactly the same order as inserted
-    const itemSharesToSave = data.items.reduce((shares, item, idx) => {
-      Object.entries(item.shares).forEach(([strUserId, strShare]) => {
-        const userId = Number(strUserId);
-        const share = Number(strShare);
-
-        if (!isNaN(userId) && !isNaN(share) && share > 0) {
-          shares.push({
-            revId: rev.id,
-            userId,
-            itemId: items[idx].id,
-            share,
-          });
-        }
+                notes: i.notes == "" ? null : i.notes,
+              })),
+            },
+          },
+        },
       });
-      return shares;
-    }, [] as TCrItemShare[]);
 
-    await ItemShare.bulkCreate(itemSharesToSave, { transaction });
+      const itemShares = await tx.itemShare.createManyAndReturn({
+        select: { itemId: true, userId: true, share: true },
+        data: data.items.flatMap((i, idx) =>
+          Object.entries(i.shares)
+            .filter(([, share]) => share !== 0)
+            .map(([uidStr, share]) => ({
+              itemId: rcpt.items[idx].id,
+              userId: Number(uidStr),
+              revisionId: rev.id!,
+              share,
+            }))
+        ),
+      });
 
-    return await rcpt.reload({ ...RCPT_INCLUDE, transaction });
-  });
+      (rcpt as TReceipt).items!.forEach(
+        (i) => (i.itemShares = itemShares.filter((sh) => sh.itemId === i.id))
+      );
+
+      (rcpt as TReceipt).archives = [];
+
+      return rcpt;
+    }
+  );
 }
 
 export async function getReceipts(userId: number, knownIds: number[] = []) {
-  return await Group.findAll({
-    include: [
-      {
-        model: Membership,
-        attributes: ["defaultCatId"],
-        where: { userId, statusId: 1 },
+  const groups = await db.group.findMany({
+    select: {
+      id: true,
+      name: true,
+      memberships: {
+        select: {
+          defaultCategoryId: true,
+          user: { select: { id: true, name: true, image: true, email: true } },
+        },
+        where: { statusId: { in: [0, 2] } },
       },
-      {
-        model: User,
-        attributes: ["id", "name", "email", "image"],
-        through: { where: { statusId: 1 }, attributes: [] },
+      categories: { select: { id: true, name: true }, where: { statusId: 0 } },
+      receipts: {
+        select: {
+          id: true,
+          revision: {
+            select: {
+              createdAtInt: true,
+              createdBy: { select: { name: true } },
+            },
+          },
+          items: true,
+          paidBy: { select: { name: true } },
+          paidOnInt: true,
+        },
+        orderBy: { paidOnInt: "desc" },
       },
-      { model: Category, attributes: ["id", "name"], where: { statusId: 1 } },
-      {
-        model: Receipt,
-        separate: true,
-        limit: 50,
-        where: { id: { [Op.not]: knownIds } },
-        ...RCPT_INCLUDE,
-        order: [["paidOn", "DESC"]],
-      } as IncludeOptions,
-    ],
-    order: [
-      fn("LOWER", col("Group.name")),
-      fn("LOWER", col("Categories.name")),
-    ],
+    },
+    where: {
+      memberships: { some: { userId } },
+    },
   });
+
+  const populateArchives = await getArchivePopulator<TReceipt>("Receipt", "id");
+
+  groups.sort(sortByName);
+  groups.forEach((g) => {
+    g.categories.sort(sortByName);
+
+    // the production-ready Prisma blindly creates parameterized SQLs (unlike Sequelize)
+    // and with 2500 receipts we already hit that wall
+    // so I need to pick the 50 first uknown receipts via post-processing
+    (g as TGroup).receipts = g.receipts.reduce((prev, curr) => {
+      if (prev.length <= 50 && !knownIds.includes(curr.id)) prev.push(curr);
+
+      return prev;
+    }, [] as TReceipt[]);
+    populateArchives(g.receipts);
+  });
+
+  return groups;
 }
