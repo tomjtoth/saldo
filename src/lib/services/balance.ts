6ba6d0c615cbbe34bb070af7fa80689fe948fe2f
Tamás Tóth <tomjtoth@gmail.com>
2025-08-11 13:57:53 +0300

    adjusted getBalance to Prisma returning 1 JSON.parseble row

--- src/lib/services/balance.ts
+++ src/lib/services/balance.ts
@@ -1,27 +1,23 @@
-import { QueryTypes } from "sequelize";
-
-import { db, TGroup } from "@/lib/models";
-import { TBalanceChartData } from "@/components/balance/chart";
+import { db, TGroup } from "@/lib/db";
+import { Prisma } from "@prisma/client";
 
 export async function getBalance(userId: number) {
-  const rows: // silencing TS, selecting only id and name from group...
-  (Omit<TGroup, "balance"> & {
-    balance: string;
-  })[] = await db.query(
-    `WITH step1 AS (
+  const rows: { json: string }[] = await db.$queryRaw(
+    Prisma.sql`
+    WITH step1 AS (
       SELECT
-        groupId AS gid,
+        ms.groupId AS gid,
         g.name AS gName,
         paidOn AS date,
-        MIN(paidBy, paidTo) AS uid1,
-        MAX(paidBy, paidTo) AS uid2,
-        SUM(share * CASE WHEN paidBy < paidTo THEN 1 ELSE -1 END) as share
-      FROM memberships ms
-      INNER JOIN consumption c ON ms.group_id = c.groupId
-      INNER JOIN groups g ON groupId = g.id
-      WHERE ms.user_id = ? AND paidBy != paidTo
-      GROUP BY groupId, paidOn, paidBy, paidTo
-      ORDER BY groupId, date
+        min(paidBy, paidTo) AS uid1,
+        max(paidBy, paidTo) AS uid2,
+        sum(share * CASE WHEN paidBy < paidTo THEN 1 ELSE -1 END) as share
+      FROM "Membership" ms
+      INNER JOIN "consumption" c ON ms.groupId = c.groupId
+      INNER JOIN "Group" g ON c.groupId = g.id
+      WHERE ms.userId = ${userId} AND paidBy != paidTo
+      GROUP BY ms.groupId, paidOn, paidBy, paidTo
+      ORDER BY ms.groupId, date
     ),
 
     step2 AS (
@@ -29,11 +25,11 @@ export async function getBalance(userId: number) {
         gid,
         gName,
         date,
-        CONCAT(u1.name, ' vs ', u2.name) AS relation,
-        SUM(share) AS share
+        concat(u1.name, ' vs ', u2.name) AS relation,
+        sum(share) AS share
       FROM step1
-      INNER JOIN users u1 ON u1.id = step1.uid1
-      INNER JOIN users u2 on u2.id = step1.uid2
+      INNER JOIN "User" u1 ON u1.id = step1.uid1
+      INNER JOIN "User" u2 on u2.id = step1.uid2
       GROUP BY gid, date, relation
     ),
 
@@ -43,32 +39,41 @@ export async function getBalance(userId: number) {
         gName,
         date,
         relation,
-        SUM(share) OVER (
+        sum(share) OVER (
           PARTITION BY gid, relation
           ROWS BETWEEN UNBOUNDED PRECEDING AND CURRENT ROW
         ) AS share
       FROM step2
       GROUP BY gid, date, relation
+    ), 
+    
+    step4 AS (
+      SELECT
+        concat(
+        '{ "id": ',
+        json_quote(gid),
+
+        ', "name": ',
+        json_quote(gName),
+
+        ', "balance": ',
+        json_object(
+          'relations',
+          json_group_array(distinct relation),
+
+          'data',
+          json_group_array(json_object('date', date, relation, round(share, 2)))
+        ),
+        
+        ' }'
+        ) AS json
+      FROM step3
+      GROUP BY gid
     )
 
-    SELECT
-    	gid AS id,
-      gName AS name,
-      JSON_OBJECT(
-        'relations',
-        JSON_GROUP_ARRAY(distinct relation),
-
-        'data',
-        JSON_GROUP_ARRAY(JSON_OBJECT('date', date, relation, share))
-      ) AS balance
-    FROM step3
-    GROUP BY gid`,
-
-    { type: QueryTypes.SELECT, replacements: [userId], nest: true }
+    SELECT concat('[',  group_concat(json), ']') AS json
+    FROM step4`
   );
 
-  return rows.map(({ balance, ...group }) => ({
-    ...group,
-    balance: JSON.parse(balance) as TBalanceChartData,
-  })) as TGroup[];
+  return rows.length > 0 ? (JSON.parse(rows[0].json) as TGroup[]) : [];
 }
