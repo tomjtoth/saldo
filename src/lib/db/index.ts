2b7d28ea93b9a9375fcd7e586a64625e495c67c2
Tamás Tóth <tomjtoth@gmail.com>
2025-08-11 13:54:37 +0300

    rm client extensions from PrismaClient

--- src/lib/db/index.ts
+++ src/lib/db/index.ts
@@ -1,30 +1,13 @@
 import { PrismaClient } from "@prisma/client";
 
-import { dateFromInt, datetimeFromInt } from "../utils";
-
 export * from "./types";
 export { migrator } from "./migrator";
 export { atomic } from "./atomic";
+export { updater } from "./updater";
+export { getArchivePopulator } from "./archives";
 
-export const db = new PrismaClient().$extends({
-  result: {
-    receipt: {
-      paidOn: {
-        needs: { paidOnInt: true },
-        compute: (rec) => dateFromInt(rec.paidOnInt),
-      },
-    },
-    revision: {
-      createdOn: {
-        needs: { createdAtInt: true },
-        compute: (rev) => datetimeFromInt(rev.createdAtInt)!,
-      },
-    },
-    membership: {
-      admin: {
-        needs: { statusId: true },
-        compute: (data) => data.statusId & 2,
-      },
-    },
-  },
-});
+// client extensions should not be used, as they choke Next.js with unserializable data
+// implemented fn asPlain() => JSON.parse(JSON.stringify(obj)), but seems like a waste of resources
+// https://github.com/prisma/prisma/issues/20627
+
+export const db = new PrismaClient({ log: ["query"] });
