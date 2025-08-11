5fb759eba87521357a5e82fa1b8f4b26a03e306b
Tamás Tóth <tomjtoth@gmail.com>
2025-08-11 13:55:54 +0300

    adjusted importV3/index to Prisma

--- src/lib/services/importV3/index.ts
+++ src/lib/services/importV3/index.ts
@@ -2,17 +2,7 @@
 
 import { db } from "@/lib/db";
 import { parseCSV, parseData, TDBData } from "./parsers";
-import {
-  Revision,
-  User,
-  Category,
-  Receipt,
-  Item,
-  ItemShare,
-  atomic,
-  Group,
-  Membership,
-} from "@/lib/models";
+import { Prisma } from "@prisma/client";
 
 export const alreadyInProd = async () => {
   const user = await db.user.findFirst({
@@ -29,39 +19,21 @@ export async function importV3() {
 }
 
 export async function insertData(data: TDBData) {
-  return await atomic("importing from V3", async (transaction) => {
-    // drop ALL imported data
-    await Revision.truncate({ transaction, cascade: true });
+  await db.$transaction([
+    db.$queryRaw(Prisma.sql`PRAGMA defer_foreign_keys = ON;`),
+    db.revision.deleteMany(),
 
-    // these 2 can be run in parallel
-    const [revisions, users] = await Promise.all([
-      Revision.bulkCreate(data.revisions, { transaction }),
-      User.bulkCreate(data.users, { transaction }),
-    ]);
+    db.revision.createMany({ data: data.revisions }),
+    db.user.createMany({ data: data.users }),
+    db.group.createMany({ data: data.groups }),
+    db.membership.createMany({ data: data.memberships }),
+    db.category.createMany({ data: data.categories }),
+    db.receipt.createMany({ data: data.receipts }),
+    db.item.createMany({ data: data.items }),
+    db.itemShare.createMany({ data: data.itemShares }),
+  ]);
 
-    // the rest are to be inserted in strict order
-    const groups = await Group.bulkCreate(data.groups, { transaction });
-    const memberships = await Membership.bulkCreate(data.memberships, {
-      transaction,
-    });
-    const categories = await Category.bulkCreate(data.categories, {
-      transaction,
-    });
-    const receipts = await Receipt.bulkCreate(data.receipts, { transaction });
-    const items = await Item.bulkCreate(data.items, { transaction });
-    const itemShares = await ItemShare.bulkCreate(data.itemShares, {
-      transaction,
-    });
-
-    return {
-      revisions: revisions.length,
-      users: users.length,
-      groups: groups.length,
-      memberships: memberships.length,
-      categories: categories.length,
-      receipts: receipts.length,
-      items: items.length,
-      itemShares: itemShares.length,
-    };
-  });
+  return Object.fromEntries(
+    Object.entries(data).map(([key, arr]) => [key, arr.length])
+  );
 }
