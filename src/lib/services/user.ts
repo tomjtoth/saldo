e7a532fcd2645a4012d70e37bf22a2c68a1bb704
Tamás Tóth <tomjtoth@gmail.com>
2025-08-11 13:56:37 +0300

    adjusted users service to Prisma

--- src/lib/services/user.ts
+++ src/lib/services/user.ts
@@ -1,15 +1,27 @@
 import { Session } from "next-auth";
 
-import { Revision, TCrUser, atomic, User } from "../models";
 import { createGroup } from "./groups";
+import { atomic, db, User } from "../db";
+import { datetimeToInt } from "../utils";
+
+type TCrUser = Pick<User, "email" | "name" | "image">;
 
 export async function addUser(userData: TCrUser) {
-  return await atomic("Adding new user", async (transaction) => {
-    const user = await User.create(userData, { transaction });
+  return await atomic({ operation: "Adding new user" }, async (tx) => {
+    const user = await tx.user.create({
+      data: {
+        ...userData,
+        revisionId: -1,
+      },
+    });
 
-    const rev = await Revision.create({ revBy: user.id }, { transaction });
+    const rev = await tx.revision.create({
+      data: { createdById: user.id, createdAtInt: datetimeToInt() },
+    });
 
-    await user.update({ revId: rev.id }, { transaction });
+    user.revisionId = rev.id;
+
+    await tx.user.update({ where: { id: user.id }, data: user });
 
     return user;
   });
@@ -19,32 +31,37 @@ export async function currentUser(session: Session) {
   // OAuth profiles without an email are disallowed in @/auth.ts
   // eslint-disable-next-line @typescript-eslint/no-non-null-asserted-optional-chain
   const email = session?.user?.email!;
-  const name = session?.user?.name ?? `User #${await User.count()}`;
-  const image = session?.user?.image;
+  const name = session?.user?.name ?? `User #${await db.user.count()}`;
+  const image = session?.user?.image ?? null;
 
-  let user = await User.findOne({ where: { email } });
+  let user = await db.user.findFirst({ where: { email } });
+
   if (!user) {
     user = await addUser({
       name,
       email,
+      image,
     });
 
     await createGroup(user.id, { name: "just you" });
   }
 
-  let updating = false;
+  const updater: Partial<User> = {};
 
   if (name !== user.name) {
-    updating = true;
-    user.name = name;
+    updater.name = user.name = name;
   }
 
   if (image && image !== user.image) {
-    updating = true;
-    user.image = image;
+    updater.image = user.image = image;
   }
 
-  if (updating) await user.save();
+  if (Object.keys(updater).length > 0) {
+    await db.user.update({
+      where: { id: user.id },
+      data: { ...updater },
+    });
+  }
 
   return user;
 }
