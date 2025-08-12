55fdf6425d9ebc8b71a47707fc68a2d00ad38b06
Tamás Tóth <tomjtoth@gmail.com>
2025-08-12 21:39:10 +0300

    migrator to use - db.$client - sql syntax highlight via 2 helper objects

--- src/lib/db/migrator.ts
+++ src/lib/db/migrator.ts
@@ -1,138 +1,116 @@
-import fs, { Dirent } from "fs";
+import fs from "fs";
 
-import { Database, OPEN_CREATE, OPEN_READWRITE } from "sqlite3";
+import { sql } from "drizzle-orm";
+
 import { err } from "../utils";
-
-const MIG_DIR = "prisma/migrations";
-const migName = (mig: Dirent<string>) =>
-  mig.parentPath.slice(MIG_DIR.length + 1);
+import { ddb as db } from ".";
 
 export const migrator = {
   up: async () => {
-    const db = new Database(
-      // file:../data/dev.db
-      process.env.DATABASE_URL?.slice(8) ?? ":memory:",
-      OPEN_CREATE | OPEN_READWRITE
-    );
+    /**
+     * simple wrapper around sql template strings with working syntax highlight (dependent on the "sql" name) in VS Code
+     */
+    const exec = {
+      async sql(strings: TemplateStringsArray) {
+        const query = strings.join(" ");
+        await db.$client.executeMultiple(query);
+      },
+    };
 
-    const query = (sql: string, values?: unknown[]) =>
-      new Promise<unknown[]>((resolve, reject) => {
-        db.all(sql, values, (err, rows) => {
-          if (err) reject(err);
-          else resolve(rows);
-        });
-      });
+    /**
+     * same as `db.all(sql`statement`)`
+     * simple wrapper around sql template strings with working syntax highlight (dependent on the "sql" name) in VS Code
+     */
+    const query = {
+      async sql(strings: TemplateStringsArray, ...values: unknown[]) {
+        const query = sql(strings, ...values);
+        return await db.all(query);
+      },
+    };
 
-    const exec = (sql: string) =>
-      new Promise<void>((resolve, reject) => {
-        db.exec(sql, (err) => {
-          if (err) reject(err);
-          else resolve();
-        });
-      });
+    const migrationsDir = "migrations";
 
-    await exec(`CREATE TABLE IF NOT EXISTS meta (
-      id INTEGER PRIMARY KEY,
-      info TEXT NOT NULL UNIQUE,
-      data BLOB
-    );`);
+    await exec.sql`
+      CREATE TABLE IF NOT EXISTS "meta" (
+        "id" INTEGER PRIMARY KEY,
+        "info" TEXT NOT NULL UNIQUE,
+        "data" BLOB
+      );
+    `;
 
     const separateMigrationsTableExists =
-      (await query("SELECT 1 FROM sqlite_master WHERE tbl_name = 'migrations'"))
-        .length > 0;
+      (
+        await query.sql`
+          SELECT 1 FROM sqlite_master 
+          WHERE tbl_name = 'migrations';
+        `
+      ).length > 0;
 
-    if (separateMigrationsTableExists) {
-      await exec(`
-        INSERT INTO meta (info, data)
-        SELECT 'migrations', json_group_array(name)
-        FROM migrations ORDER BY name;
+    if (separateMigrationsTableExists)
+      await exec.sql`
+        INSERT INTO "meta" ("info", "data")
+        SELECT 'migrations', json_group_array("name")
+        FROM "migrations" ORDER BY "name";
 
-        DROP TABLE migrations;
-      `);
-    }
+        DROP TABLE "migrations";
+      `;
 
     const done = (
-      (await query(
-        "SELECT value FROM meta, json_each(data) WHERE info = 'migrations'"
-      )) as { value: string }[]
-    ).map((x) => x.value);
+      await query.sql`
+        SELECT value FROM meta, json_each(data)
+        WHERE info = 'migrations';
+      `
+    ).map((x) => (x as { value: string }).value);
 
     const migrations = fs
-      .readdirSync(MIG_DIR, {
-        recursive: true,
-        withFileTypes: true,
-        encoding: "utf-8",
-      })
-      .filter(
-        (file) =>
-          !file.parentPath.endsWith("20250708_pre-prisma") &&
-          file.name.endsWith(".sql") &&
-          // without the prisma/migrations path
-          !done.includes(migName(file))
-      );
+      .readdirSync(migrationsDir, { encoding: "utf-8" })
+      .filter((file) => file.endsWith(".sql") && !done.includes(file));
 
     const res: string[] = [];
 
-    const trimmer = new RegExp(
-      [
-        "^ *(?:",
-
-        [
-          "BEGIN *(?:DEFERRED|IMMEDIATE|EXCLUSIVE)? *(?:TRANSACTION)?",
-          "(?:COMMIT|END) *(?:TRANSACTION)?",
-          "VACUUM",
-        ].join("|"),
-
-        ") *; *$",
-      ].join(""),
-      "mig"
-    );
-
     try {
-      for (const mig of migrations) {
-        const script = fs.readFileSync(`${mig.parentPath}/${mig.name}`, {
+      for (const migName of migrations) {
+        const script = fs.readFileSync(`${migrationsDir}/${migName}`, {
           encoding: "utf-8",
         });
 
-        const trimmed = script.replaceAll(trimmer, "");
-
-        await exec("PRAGMA foreign_keys = OFF;");
-        await exec("BEGIN;");
+        await exec.sql`PRAGMA foreign_keys = OFF`;
+        const tx = await db.$client.transaction();
 
         try {
-          await exec(trimmed);
+          await tx.executeMultiple(script);
 
-          const violations = await query("PRAGMA foreign_key_check;");
-          const len = violations.length;
+          const violations = await tx.execute("PRAGMA foreign_key_check");
+          const len = violations.rows.length;
 
           if (len > 0)
             err(
-              `${len} foreign_key violations occured during "${
-                mig.parentPath
-              }":\n\n${JSON.stringify(violations, null, "\t")}\n`
+              `${len} foreign_key violations occured during "${migName}":\n\n${JSON.stringify(
+                violations.rows,
+                null,
+                "\t"
+              )}\n`
             );
 
-          await exec("COMMIT;");
+          await tx.commit();
 
-          await query(
-            `INSERT INTO meta (info, data) VALUES ('migrations', json_array(?1))
-            ON CONFLICT DO UPDATE SET data = json_insert(data, '$[#]', ?1)`,
-            [migName(mig)]
-          );
+          await query.sql`
+            INSERT INTO meta (info, data) VALUES ('migrations', json_array(${migName}))
+            ON CONFLICT DO UPDATE SET data = json_insert(data, '$[#]', ${migName});
+          `;
         } catch (err) {
-          await exec("ROLLBACK;");
+          await tx.rollback();
           throw err;
         } finally {
-          await exec("PRAGMA foreign_keys = ON;");
+          await exec.sql`PRAGMA foreign_keys = ON`;
         }
 
-        res.push(migName(mig));
+        res.push(migName);
       }
     } catch (err) {
       throw err;
     } finally {
-      await exec("VACUUM");
-      db.close();
+      await exec.sql`VACUUM`;
     }
 
     return res;
