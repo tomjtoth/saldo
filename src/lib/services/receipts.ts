1d4974a97f1d4198ec8d173bf993d3d5cd0192b9
Tamás Tóth <tomjtoth@gmail.com>
2025-08-13 22:03:56 +0300

    migrated getReceipts to Drizzle

--- src/lib/services/receipts.ts
+++ src/lib/services/receipts.ts
@@ -89,50 +89,7 @@ export async function addReceipt(addedBy: number, data: TReceiptInput) {
 }
 
 export async function getReceipts(userId: number, knownIds: number[] = []) {
-  const groups = await db.group.findMany({
-    select: {
-      id: true,
-      name: true,
-      memberships: {
-        select: {
-          defaultCategoryId: true,
-          user: { select: { id: true, name: true, image: true, email: true } },
-        },
-        where: { statusId: { in: [0, 2] } },
-      },
-      categories: { select: { id: true, name: true }, where: { statusId: 0 } },
-      receipts: {
-        select: {
-          id: true,
-          revision: {
-            select: {
-              createdAtInt: true,
-              createdBy: { select: { name: true } },
-            },
-          },
-          items: true,
-          paidBy: { select: { name: true } },
-          paidOnInt: true,
-        },
-        orderBy: { paidOnInt: "desc" },
-      },
-    },
-    where: {
-      memberships: { some: { userId } },
-    },
-  });
-
-  // drizzle
-  await ddb.query.groups.findMany({
-    where: (t, { exists, eq, and }) =>
-      exists(
-        ddb
-          .select()
-          .from(memberships)
-          .where(
-            and(eq(memberships.groupId, t.id), eq(memberships.userId, userId))
-          )
-      ),
+  const groups = (await db.query.groups.findMany({
     columns: {
       id: true,
       name: true,
@@ -145,54 +102,39 @@ export async function getReceipts(userId: number, knownIds: number[] = []) {
         with: {
           user: { columns: { id: true, name: true, image: true, email: true } },
         },
-        where: (t, op) => op.inArray(t.statusId, [0, 2]),
+        where: isActive,
       },
       categories: {
         columns: { id: true, name: true },
-        where: (t, op) => op.eq(t.statusId, 0),
-        orderBy: (t, op) => op.sql`lower(${t.name})`,
+        where: isActive,
+        orderBy: orderByLowerName,
       },
       receipts: {
-        columns: {
-          id: true,
-          paidOn: true,
-        },
+        ...RECEIPT_COLS_WITH,
         limit: 50,
-        with: {
-          revision: {
-            columns: {
-              createdAt: true,
-            },
-            with: {
-              createdBy: { columns: { name: true } },
-            },
-          },
-          items: true,
-          paidBy: { columns: { name: true } },
-        },
         where: (t, op) =>
           op.sql`${t.id} not in ${op.sql.raw("(" + knownIds.join(", ") + ")")}`,
         orderBy: (t, op) => op.desc(t.paidOn),
       },
     },
+    where: (t, { exists, eq, and }) =>
+      exists(
+        db
+          .select()
+          .from(memberships)
+          .where(
+            and(eq(memberships.groupId, t.id), eq(memberships.userId, userId))
+          )
+      ),
     orderBy: (t, op) => op.sql`lower(${t.name})`,
-  });
+  })) as TGroup[];
 
   const populateArchives = await getArchivePopulator<TReceipt>("Receipt", "id");
 
   groups.sort(sortByName);
   groups.forEach((g) => {
-    g.categories.sort(sortByName);
-
-    // the production-ready Prisma blindly creates parameterized SQLs (unlike Sequelize)
-    // and with 2500 receipts we already hit that wall
-    // so I need to pick the 50 first uknown receipts via post-processing
-    (g as TGroup).receipts = g.receipts.reduce((prev, curr) => {
-      if (prev.length <= 50 && !knownIds.includes(curr.id)) prev.push(curr);
-
-      return prev;
-    }, [] as TReceipt[]);
-    populateArchives(g.receipts);
+    g.categories!.sort(sortByName);
+    populateArchives(g.receipts!);
   });
 
   return groups;
