b25d777297c62dfeb76316c68fbbfc7e11520857
Tamás Tóth <tomjtoth@gmail.com>
2025-08-13 18:30:48 +0300

    migrated import component to Drizzle

--- src/components/import/index.tsx
+++ src/components/import/index.tsx
@@ -1,6 +1,6 @@
 "use server";
 
-import { db } from "@/lib/db";
+import { ddb as db, schema } from "@/lib/db";
 import { alreadyInProd } from "@/lib/services/importV3";
 
 import CliImportSection from "./clientSide";
@@ -19,14 +19,14 @@ export default async function ImportSection() {
       items,
       itemShares,
     ] = await Promise.all([
-      db.user.count(),
-      db.revision.count(),
-      db.group.count(),
-      db.membership.count(),
-      db.category.count(),
-      db.receipt.count(),
-      db.item.count(),
-      db.itemShare.count(),
+      db.$count(schema.users),
+      db.$count(schema.revisions),
+      db.$count(schema.groups),
+      db.$count(schema.memberships),
+      db.$count(schema.categories),
+      db.$count(schema.receipts),
+      db.$count(schema.items),
+      db.$count(schema.itemShares),
     ]);
 
     rendered = (
