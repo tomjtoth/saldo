d530aa25563849d99982e3a991a4c8a84378ddf4
Tamás Tóth <tomjtoth@gmail.com>
2025-08-13 19:08:03 +0300

    importer/insertData migreated to Drizzle

--- src/lib/services/importV3/index.ts
+++ src/lib/services/importV3/index.ts
@@ -19,19 +19,28 @@ export async function importV3() {
 }
 
 export async function insertData(data: TDBData) {
-  await db.$transaction([
-    db.$queryRaw(Prisma.sql`PRAGMA defer_foreign_keys = ON;`),
-    db.revision.deleteMany(),
+  await db.transaction(async (tx) => {
+    await tx.delete(schema.revisions);
 
-    db.revision.createMany({ data: data.revisions }),
-    db.user.createMany({ data: data.users }),
-    db.group.createMany({ data: data.groups }),
-    db.membership.createMany({ data: data.memberships }),
-    db.category.createMany({ data: data.categories }),
-    db.receipt.createMany({ data: data.receipts }),
-    db.item.createMany({ data: data.items }),
-    db.itemShare.createMany({ data: data.itemShares }),
-  ]);
+    await tx.run("PRAGMA defer_foreign_keys = ON");
+
+    await tx.insert(schema.revisions).values(data.revisions);
+    await tx.insert(schema.users).values(data.users);
+    await tx.insert(schema.groups).values(data.groups);
+    await tx.insert(schema.memberships).values(data.memberships);
+    await tx.insert(schema.categories).values(data.categories);
+    await tx.insert(schema.receipts).values(data.receipts);
+
+    await inChunks(
+      (chunk) => tx.insert(schema.items).values(chunk),
+      data.items
+    );
+
+    await inChunks(
+      (chunk) => tx.insert(schema.itemShares).values(chunk),
+      data.itemShares
+    );
+  });
 
   return Object.fromEntries(
     Object.entries(data).map(([key, arr]) => [key, arr.length])
