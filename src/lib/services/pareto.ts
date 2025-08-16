016a906bd490dd387fcbe2d79743af73a8184e6f
Tamás Tóth <tomjtoth@gmail.com>
2025-08-16 15:37:51 +0300

    implemented consumption and the 1st CTE from pareto

--- src/lib/services/pareto.ts
+++ src/lib/services/pareto.ts
@@ -1,8 +1,17 @@
 import { DateTime } from "luxon";
-import { sql } from "drizzle-orm";
+import { sql, eq, and } from "drizzle-orm";
 
-import { db, TGroup } from "@/lib/db";
+import { db, isActive, TGroup } from "@/lib/db";
 import { dateToInt, EUROPE_HELSINKI } from "../utils";
+import {
+  categories,
+  groups,
+  items,
+  itemShares,
+  memberships,
+  receipts,
+  users,
+} from "../db/schema";
 
 export async function getPareto(
   userId: number,
@@ -25,6 +34,74 @@ export async function getPareto(
       ? `AND paidOn < ${dateToInt(opts.to)}`
       : "";
 
+  const consumption = db.$with("consumption").as((qb) =>
+    qb
+      .select({
+        groupId: sql<number>`${receipts.groupId}`.as("consumption_group_id"),
+        paidOn: sql<number>`${receipts.paidOn}`.as("consumption_paid_on"),
+        paidBy: sql<number>`${receipts.paidById}`.as("consumption_paid_by"),
+        paidTo:
+          sql<number>`coalesce(${itemShares.userId}, ${receipts.paidById})`.as(
+            "consumption_paid_to"
+          ),
+        itemId: sql<number>`${items.id}`.as("consumption_item_id"),
+        categoryId: sql<number>`${items.categoryId}`.as(
+          "consumption_category_id"
+        ),
+        share: sql<number>`${items.cost} / 100.0 * coalesce(
+        ${itemShares.share} * 1.0 / sum(${itemShares.share}) OVER (PARTITION BY ${items.id}), 1
+      )`.as("consumption_share"),
+      })
+      .from(receipts)
+      .innerJoin(items, and(eq(receipts.id, items.receiptId), isActive(items)))
+      .leftJoin(
+        itemShares,
+        and(eq(items.id, itemShares.itemId), isActive(itemShares))
+      )
+      .where(isActive(receipts))
+      .orderBy(receipts.paidOn)
+  );
+
+  const sumsPerRow = db.$with("sums_per_row").as((qb) =>
+    qb
+      .select({
+        groupId: sql<number>`${groups.id}`.as("cte1_group_id"),
+        groupName: sql<string>`${groups.name}`.as("group_name"),
+        categoryName: sql<string>`${categories.name}`.as("category_name"),
+        total: sql<number>`sum(${consumption.share})`.as("total"),
+      })
+      .from(memberships)
+      .innerJoin(consumption, eq(memberships.groupId, consumption.groupId))
+      .innerJoin(categories, eq(categories.id, consumption.categoryId))
+      .innerJoin(users, eq(users.id, consumption.paidTo))
+      .innerJoin(groups, eq(groups.id, consumption.groupId))
+      .where(
+        and(
+          eq(users.id, userId),
+
+          opts.from &&
+            // SQL injection prevented here
+            DateTime.fromISO(opts.from, EUROPE_HELSINKI).isValid
+            ? sql`${consumption.paidOn} > ${dateToInt(opts.from)}`
+            : undefined,
+
+          opts.to &&
+            // SQL injection prevented here
+            DateTime.fromISO(opts.to, EUROPE_HELSINKI).isValid
+            ? sql`${consumption.paidOn} < ${dateToInt(opts.to)}`
+            : undefined
+        )
+      )
+      .groupBy(
+        sql`${sql.identifier("cte1_group_id")}`,
+        sql`${consumption.paidTo}`,
+        sql`${consumption.categoryId}`
+      )
+  );
+
+  const test = await db.with(consumption, sumsPerRow).select().from(sumsPerRow);
+  console.log(test);
+
   const data = await db.get<{ json: string } | null>(
     sql`WITH sums_per_row AS (
       SELECT
@@ -33,12 +110,12 @@ export async function getPareto(
         cats.name AS cat,
         u.name AS user,
         sum(share) AS total
-      FROM "Membership" ms
-      INNER JOIN consumption con ON ms.groupId = con.groupId
-      INNER JOIN "Category" cats ON con.categoryId = cats.id
-      INNER JOIN "User" u ON u.id = con.paidTo
-      INNER JOIN "Group" g ON g.id = con.groupId
-      WHERE ms.userId = ${userId} ${from} ${to}
+      FROM "memberships" ms
+      INNER JOIN consumption con ON ms.group_id = con.groupId
+      INNER JOIN "categories" cats ON con.categoryId = cats.id
+      INNER JOIN "users" u ON u.id = con.paidTo
+      INNER JOIN "groups" g ON g.id = con.groupId
+      WHERE ms.user_id = ${userId} ${sql.raw(`${from} ${to}`)}
       GROUP BY g.id, paidTo, categoryId
     ),
 
