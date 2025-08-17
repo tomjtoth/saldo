1a6b49b5a53dbfcffeede35b43baf4fe8db73b4f
Tamás Tóth <tomjtoth@gmail.com>
2025-08-17 22:03:49 +0300

    migrated api/import/POST to protectedRoute

--- src/app/api/import/route.ts
+++ src/app/api/import/route.ts
@@ -1,20 +1,11 @@
+import protectedRoute from "@/lib/protectedRoute";
 import { alreadyInProd, importV3 } from "@/lib/services/importV3";
+import { err } from "@/lib/utils";
 
-export async function POST() {
-  if (await alreadyInProd())
-    return new Response(null, {
-      status: 403,
-      statusText: "already in production",
-    });
+export const POST = protectedRoute(async () => {
+  if (await alreadyInProd()) err(403, "already in production");
 
-  try {
-    const imported = await importV3();
+  const imported = await importV3();
 
-    return Response.json(imported);
-  } catch (err) {
-    return new Response(null, {
-      status: 400,
-      statusText: (err as Error).message,
-    });
-  }
-}
+  return Response.json(imported);
+});
