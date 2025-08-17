27496548010856f907f098e924870e76e2827d04
Tamás Tóth <tomjtoth@gmail.com>
2025-08-17 12:14:16 +0300

    initial support for migrating up/down

--- src/lib/db/migrator.ts
+++ src/lib/db/migrator.ts
@@ -5,32 +5,31 @@ import { sql } from "drizzle-orm";
 import { err } from "../utils";
 import { db } from ".";
 
-export const migrator = {
-  up: async () => {
-    /**
-     * simple wrapper around sql template strings with working syntax highlight (dependent on the "sql" name) in VS Code
-     */
-    const exec = {
-      async sql(strings: TemplateStringsArray) {
-        const query = strings.join(" ");
-        await db.$client.executeMultiple(query);
-      },
-    };
+const migrate = (direcionIsUp: boolean) => async () => {
+  /**
+   * simple wrapper around sql template strings with working syntax highlight (dependent on the "sql" name) in VS Code
+   */
+  const exec = {
+    async sql(strings: TemplateStringsArray) {
+      const query = strings.join(" ");
+      await db.$client.executeMultiple(query);
+    },
+  };
 
-    /**
-     * same as `db.all(sql`statement`)`
-     * simple wrapper around sql template strings with working syntax highlight (dependent on the "sql" name) in VS Code
-     */
-    const query = {
-      async sql(strings: TemplateStringsArray, ...values: unknown[]) {
-        const query = sql(strings, ...values);
-        return await db.all(query);
-      },
-    };
+  /**
+   * same as `db.all(sql`statement`)`
+   * simple wrapper around sql template strings with working syntax highlight (dependent on the "sql" name) in VS Code
+   */
+  const query = {
+    async sql(strings: TemplateStringsArray, ...values: unknown[]) {
+      const query = sql(strings, ...values);
+      return await db.all(query);
+    },
+  };
 
-    const migrationsDir = "migrations";
+  const migrationsDir = "migrations";
 
-    await exec.sql`
+  await exec.sql`
       CREATE TABLE IF NOT EXISTS "metadata" (
         "id" INTEGER PRIMARY KEY,
         "name" TEXT NOT NULL UNIQUE,
@@ -39,16 +38,16 @@ export const migrator = {
       );
     `;
 
-    const separateMigrationsTableExists =
-      (
-        await query.sql`
+  const separateMigrationsTableExists =
+    (
+      await query.sql`
           SELECT 1 FROM sqlite_master 
           WHERE tbl_name = 'migrations';
         `
-      ).length > 0;
+    ).length > 0;
 
-    if (separateMigrationsTableExists)
-      await exec.sql`
+  if (separateMigrationsTableExists)
+    await exec.sql`
         INSERT INTO "metadata" ("name", "payload")
         SELECT 'migrations', json_group_array("name")
         FROM "migrations" ORDER BY "name";
@@ -56,64 +55,75 @@ export const migrator = {
         DROP TABLE "migrations";
       `;
 
-    const done = (
-      await query.sql`
+  const done = (
+    await query.sql`
         SELECT "value" FROM "metadata", json_each("payload")
         WHERE "name" = 'migrations';
       `
-    ).map((x) => (x as { value: string }).value);
+  ).map((x) => (x as { value: string }).value);
 
-    const migrations = fs
-      .readdirSync(migrationsDir, { encoding: "utf-8" })
-      .filter((file) => file.endsWith(".sql") && !done.includes(file));
+  const migrations = fs
+    .readdirSync(migrationsDir, { encoding: "utf-8" })
+    .filter((file) => file.endsWith(".sql") && !done.includes(file));
 
-    const res: string[] = [];
+  const res: string[] = [];
 
-    try {
-      for (const migName of migrations) {
-        const script = fs.readFileSync(`${migrationsDir}/${migName}`, {
-          encoding: "utf-8",
-        });
+  const splitter = /(.+)^--\s*DOWN\s*--$(.*)/ms;
 
-        await exec.sql`PRAGMA foreign_keys = OFF`;
-        const tx = await db.$client.transaction();
+  try {
+    for (const migName of migrations) {
+      const script = fs.readFileSync(`${migrationsDir}/${migName}`, {
+        encoding: "utf-8",
+      });
 
-        try {
-          await tx.executeMultiple(script);
+      const split = script.match(splitter);
+      if (!split) err("missing DOWN part of migration");
 
-          const violations = await tx.execute("PRAGMA foreign_key_check");
-          const len = violations.rows.length;
+      const [, up, down] = split;
 
-          if (len > 0)
-            err(
-              `${len} foreign_key violations occured during "${migName}":\n\n${JSON.stringify(
-                violations.rows,
-                null,
-                "\t"
-              )}\n`
-            );
+      await exec.sql`PRAGMA foreign_keys = OFF`;
+      const tx = await db.$client.transaction();
 
-          await tx.commit();
+      try {
+        await tx.executeMultiple(direcionIsUp ? up : down);
 
-          await query.sql`
+        const violations = await tx.execute("PRAGMA foreign_key_check");
+        const len = violations.rows.length;
+
+        if (len > 0)
+          err(
+            `${len} foreign_key violations occured during "${migName}":\n\n${JSON.stringify(
+              violations.rows,
+              null,
+              "\t"
+            )}\n`
+          );
+
+        await tx.commit();
+
+        await query.sql`
             INSERT INTO "metadata" ("name", "payload") VALUES ('migrations', json_array(${migName}))
             ON CONFLICT DO UPDATE SET "payload" = json_insert("payload", '$[#]', ${migName});
           `;
-        } catch (err) {
-          await tx.rollback();
-          throw err;
-        } finally {
-          await exec.sql`PRAGMA foreign_keys = ON`;
-        }
-
-        res.push(migName);
+      } catch (err) {
+        await tx.rollback();
+        throw err;
+      } finally {
+        await exec.sql`PRAGMA foreign_keys = ON`;
       }
-    } catch (err) {
-      throw err;
-    } finally {
-      await exec.sql`VACUUM`;
-    }
 
-    return res;
-  },
+      res.push(migName);
+    }
+  } catch (err) {
+    throw err;
+  } finally {
+    await exec.sql`VACUUM`;
+  }
+
+  return res;
+};
+
+export const migrator = {
+  up: migrate(true),
+  down: migrate(false),
 };
