e5b4cb260cac3abd3aa9a58b1487abd29bfafba7
Tamás Tóth <tomjtoth@gmail.com>
2025-08-17 21:55:04 +0300

    use new err(status) in protectedRoute

--- src/lib/protectedRoute.ts
+++ src/lib/protectedRoute.ts
@@ -2,6 +2,7 @@ import { NextRequest } from "next/server";
 
 import { auth } from "@/auth";
 import { currentUser } from "@/lib/services/user";
+import { err, ErrorWithStatus } from "./utils";
 
 export interface ReqWithUser extends NextRequest {
   __user: Awaited<ReturnType<typeof currentUser>>;
@@ -11,15 +12,15 @@ export default function protectedRoute(
   routeLogic: (req: ReqWithUser) => Promise<Response>
 ) {
   return async (req: NextRequest) => {
-    const sess = await auth();
-    if (!sess) return new Response(null, { status: 401 });
-    (req as ReqWithUser).__user = await currentUser(sess);
-
     try {
+      const sess = await auth();
+      if (!sess) err(401);
+      (req as ReqWithUser).__user = await currentUser(sess);
+
       return await routeLogic(req as ReqWithUser);
     } catch (err: unknown) {
       return new Response(null, {
-        status: 400,
+        status: (err as ErrorWithStatus).status ?? 400,
         statusText: (err as Error).message,
       });
     }
