85478b3e9e6978a7fd9f96971361d22846837078
Tamás Tóth <tomjtoth@gmail.com>
2025-08-18 17:52:49 +0300

    show chartLineConfig in Sidepanel

--- src/components/sidepanel/clientSide.tsx
+++ src/components/sidepanel/clientSide.tsx
@@ -5,6 +5,7 @@ import { ReactNode, useState } from "react";
 
 import { LINKS } from "./config";
 import Canceler from "../canceler";
+import ChartLineConfig from "../chartLineConfig";
 
 const hrefToLabel = (href: string) => href.replaceAll(/[^\w]+/g, "");
 
@@ -28,6 +29,7 @@ export default function CliSidepanel(srv: {
           {srv.greeter}
           {srv.signOutButton}
         </div>
+        <ChartLineConfig />
         <hr />
         <ul className="ml-2">
           {LINKS.map((a) => (
