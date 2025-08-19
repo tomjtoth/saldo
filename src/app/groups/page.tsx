610a2b92708bbeea3927fcb89210c2a3cae98b57
Tamás Tóth <tomjtoth@gmail.com>
2025-08-19 13:52:14 +0300

    adjusted groups page to smarter protectedPage

--- src/app/groups/page.tsx
+++ src/app/groups/page.tsx
@@ -1,21 +1,15 @@
 import { getGroups } from "@/lib/services/groups";
 
-import protectedPage, { TPage } from "@/lib/protectedPage";
+import protectedPage from "@/lib/protectedPage";
 import CliGroupsPage from "@/components/groups";
 
-export default ({ params }: TPage) =>
-  protectedPage({
-    params,
-    resolveParams: ({ groupId }) => {
-      const asNum = Number(groupId);
+export default protectedPage({
+  resolveParams: ({ groupId }) => ({
+    redirectTo: groupId ? `/groups/${groupId}` : "/groups",
+    groupId,
+  }),
 
-      return {
-        redirectTo: groupId ? `/groups/${groupId}` : "/groups",
-        groupId: isNaN(asNum) ? undefined : asNum,
-      };
-    },
-
-    getData: getGroups,
-    children: <CliGroupsPage />,
-    rewritePath: "/groups",
-  });
+  getData: getGroups,
+  children: <CliGroupsPage />,
+  rewritePath: "/groups",
+});
