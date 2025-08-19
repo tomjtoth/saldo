1db3e66208d5eca22d8eab88c62d9293ef74ee9a
Tamás Tóth <tomjtoth@gmail.com>
2025-08-19 13:51:55 +0300

    adjusted balance page to smarter protectedPage

--- src/app/balance/page.tsx
+++ src/app/balance/page.tsx
@@ -1,12 +1,10 @@
 import { getBalance } from "@/lib/services/balance";
 
-import protectedPage, { TPage } from "@/lib/protectedPage";
+import protectedPage from "@/lib/protectedPage";
 import CliBalancePage from "@/components/balance";
 
-export default ({ params }: TPage) =>
-  protectedPage({
-    params,
-    getData: getBalance,
-    children: <CliBalancePage />,
-    rewritePath: "/balance",
-  });
+export default protectedPage({
+  getData: getBalance,
+  children: <CliBalancePage />,
+  rewritePath: "/balance",
+});
