73db280a5aa45dd64ec8d92898737b25ae734ac4
Tamás Tóth <tomjtoth@gmail.com>
2025-08-19 13:53:20 +0300

    adjusted receipts page to smarter protectedPage

--- src/app/receipts/page.tsx
+++ src/app/receipts/page.tsx
@@ -1,12 +1,10 @@
 import { getReceipts } from "@/lib/services/receipts";
 
-import protectedPage, { TPage } from "@/lib/protectedPage";
+import protectedPage from "@/lib/protectedPage";
 import CliReceiptsPage from "@/components/receipts";
 
-export default ({ params }: TPage) =>
-  protectedPage({
-    params,
-    getData: getReceipts,
-    children: <CliReceiptsPage />,
-    rewritePath: "/receipts",
-  });
+export default protectedPage({
+  getData: getReceipts,
+  children: <CliReceiptsPage />,
+  rewritePath: "/receipts",
+});
