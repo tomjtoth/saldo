fadd6e566ec21ee716b647f3e211ee2bdac9d8e3
Tamás Tóth <tomjtoth@gmail.com>
2025-09-18 18:48:30 +0300

    `flags` related rename in metadata

--- migrations/14-rename flags in all tables.sql
+++ migrations/14-rename flags in all tables.sql
@@ -6,6 +6,10 @@ ALTER TABLE receipts RENAME status_id TO flags;
 ALTER TABLE items RENAME status_id TO flags;
 ALTER TABLE item_shares RENAME status_id TO flags;
 
+UPDATE metadata SET name = 'flags' WHERE id = 3;
+-- table_column_ids
+UPDATE metadata SET payload = replace(payload, '.statusId"', '.flags"') WHERE id = 4;
+
 -- DOWN --
 
 ALTER TABLE users RENAME flags TO status_id;
@@ -15,3 +19,6 @@ ALTER TABLE categories RENAME flags TO status_id;
 ALTER TABLE receipts RENAME flags TO status_id;
 ALTER TABLE items RENAME flags TO status_id;
 ALTER TABLE item_shares RENAME flags TO status_id;
+
+UPDATE metadata SET name = 'statuses' WHERE id = 3;
+UPDATE metadata SET payload = replace(payload, '.flags"', '.statusId"') WHERE id = 4;
