efeb3d2d296e560c87731b8063ee35cf36c0c8f2
Tamás Tóth <tomjtoth@gmail.com>
2025-10-03 14:11:35 +0300

    dropped 2 cols from users & memberships, created table chart_colors

---
+++ migrations/16-chart_colors_as_separate_table.sql
@@ -0,0 +1,13 @@
+-- will not be tracked by revisions
+CREATE TABLE chart_colors (
+    id INTEGER PRIMARY KEY,
+    user_id INTEGER NOT NULL REFERENCES users(id) ON UPDATE CASCADE,
+    group_id INTEGER REFERENCES groups(id) ON UPDATE CASCADE,
+    member_id INTEGER REFERENCES users(id) ON UPDATE CASCADE,
+
+    -- store hex of colors as int, then parse with base 16
+    color INTEGER NOT NULL
+);
+
+ALTER TABLE users DROP COLUMN chart_style;
+ALTER TABLE memberships DROP COLUMN chart_style;
