07e3ef4e58447a13ae689f58f124d6b277171a01
Tamás Tóth <tomjtoth@gmail.com>
2025-10-12 14:33:25 +0300

    added ON DELETE CASCADE to latest migration

--- migrations/16-chart_colors_as_separate_table.sql
+++ migrations/16-chart_colors_as_separate_table.sql
@@ -1,9 +1,9 @@
 -- will not be tracked by revisions
 CREATE TABLE chart_colors (
     id INTEGER PRIMARY KEY,
-    user_id INTEGER NOT NULL REFERENCES users(id) ON UPDATE CASCADE,
-    group_id INTEGER REFERENCES groups(id) ON UPDATE CASCADE,
-    member_id INTEGER REFERENCES users(id) ON UPDATE CASCADE,
+    user_id INTEGER NOT NULL REFERENCES users(id) ON UPDATE CASCADE ON DELETE CASCADE,
+    group_id INTEGER REFERENCES groups(id) ON UPDATE CASCADE ON DELETE CASCADE,
+    member_id INTEGER REFERENCES users(id) ON UPDATE CASCADE ON DELETE CASCADE,
 
     -- store hex of colors as int, then parse with base 16
     color INTEGER NOT NULL
