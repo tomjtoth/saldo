65e5a29ebb70da68ba375c39ed6ecc0f21336bc0
Tamás Tóth <tomjtoth@gmail.com>
2025-10-13 13:19:40 +0300

    Revert "tried adding e2e coverage, failed"
    This reverts commit 54aad8181a33851b0cd59347d78d6330ccb2077b.

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,14 +1,8 @@
 import { defineConfig } from "cypress";
-import codeCoverage from "@cypress/code-coverage/task";
 
 export default defineConfig({
-  // setupNodeEvents can be defined in either
-  // the e2e or component configuration
   e2e: {
     baseUrl: "http://localhost:3000",
-    setupNodeEvents(on, config) {
-      codeCoverage(on, config);
-      return config;
-    },
+    setupNodeEvents(on, config) {},
   },
 });
