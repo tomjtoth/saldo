abc2f8bf6852fc40ffa59f0e206249c463700b81
Tamás Tóth <tomjtoth@gmail.com>
2025-10-13 11:37:01 +0300

    updated exposed entities object under utils.cy

--- cypress/e2e/utils.cy.ts
+++ cypress/e2e/utils.cy.ts
@@ -17,48 +17,50 @@ export function successfulToastShouldNotExist() {
  * @param entity `"category"` | `"group"`
  * @returns an object with methods to add/update/open entities
  */
-export const methodsOf = (entity: "category" | "group") => ({
+export const entities = {
   add(name: string, description?: string) {
     // React re-render was dismissing my click event below
     cy.wait(500);
-    cy.get(`#${entity}-adder-opener`).click();
+    cy.get("#entity-adder-button").click();
 
-    cy.get(`#${entity}-adder-form > input`).type(name);
-
-    if (description)
-      cy.get(`#${entity}-adder-form > textarea`).type(description);
-    cy.get(`#${entity}-adder-form > button`).click();
+    if (name) cy.get("#entity-adder-form > input").type(name);
+    if (description) cy.get("#entity-adder-form > textarea").type(description);
+    cy.get("#entity-adder-form > button").click();
 
     successfulToastShwon(`Saving "${name}" to db succeeded!`);
   },
 
-  update({
-    name,
-    description,
-    toggle,
-  }: {
-    name?: string;
-    description?: string;
-    toggle?: true;
-  }) {
-    cy.get(`div.${entity}`).click();
+  update(
+    text: string,
+    {
+      name,
+      description,
+      toggle,
+    }: {
+      name?: string;
+      description?: string;
+      toggle?: true;
+    }
+  ) {
+    cy.contains(text)
+      .filter((_, el) => el.textContent?.trim() === text)
+      .click();
 
-    if (name) cy.get(`#${entity}-updater-form > input`).type(name);
-    if (description)
-      cy.get(`#${entity}-updater-form > textarea`).type(description);
+    if (name) cy.get("#updater > input").type(name);
+    if (description) cy.get("#updater > textarea").type(description);
     if (toggle) this.toggler.click();
 
-    cy.get(`#${entity}-updater-form > button`).click();
+    cy.get("#updater>button").click();
   },
 
   get toggler() {
-    return cy.get(`#${entity}-updater-form > div`).first();
+    return cy.get("#updater > div").first();
   },
 
   shouldBeFavorit(name: string) {
     cy.contains(name).find("svg g[fill='#FB0']").should("exist");
   },
-});
+};
 
 export const cleanup = () => cy.request("/api/cleanup");
 
