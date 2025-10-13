93692a333bf8ba02ffc1aa6e4ade8bab878e87df
Tamás Tóth <tomjtoth@gmail.com>
2025-10-13 11:37:52 +0300

    updated categories.cy according to the new entities in utils.cy

--- cypress/e2e/categories.cy.ts
+++ cypress/e2e/categories.cy.ts
@@ -2,14 +2,11 @@ import { login, loginShouldBeVisible } from "./session.cy";
 import {
   accessibleViaSidepanel,
   cleanup,
-  methodsOf,
+  entities,
   selectGroup,
   successfulToastShouldNotExist,
   successfulToastShwon,
 } from "./utils.cy";
-
-const cats = methodsOf("category");
-const groups = methodsOf("group");
 
 const TEST_CATEGORY = `category-${Date.now()}`;
 
@@ -25,66 +22,66 @@ describe("categories", () => {
     accessibleViaSidepanel("/categories");
 
     it("can be added", () => {
-      cats.add(TEST_CATEGORY);
+      entities.add(TEST_CATEGORY);
     });
 
     it("can be renamed", () => {
-      cats.add(TEST_CATEGORY);
+      entities.add(TEST_CATEGORY);
 
-      cats.update({ name: "-2" });
+      entities.update(TEST_CATEGORY, { name: "-2" });
 
       successfulToastShwon(`Renaming "${TEST_CATEGORY}" succeeded!`);
     });
 
     it("can be toggled", () => {
-      cats.add(TEST_CATEGORY);
+      entities.add(TEST_CATEGORY);
 
-      cats.update({ toggle: true });
+      entities.update(TEST_CATEGORY, { toggle: true });
 
       successfulToastShwon(`Toggling "${TEST_CATEGORY}" succeeded!`);
-      cats.toggler.should("have.class", "bg-red-500");
-      cats.toggler.parent().should("have.class", "border-red-500");
+      entities.toggler.should("have.class", "bg-red-500");
+      entities.toggler.parent().should("have.class", "border-red-500");
     });
 
     describe("can be set as favorit", () => {
       it("but not if they're already set favorit", () => {
-        cats.add(TEST_CATEGORY);
+        entities.add(TEST_CATEGORY);
 
         cy.contains(TEST_CATEGORY).find("svg").click();
         successfulToastShwon("Setting default category succeeded!");
-        cats.shouldBeFavorit(TEST_CATEGORY);
+        entities.shouldBeFavorit(TEST_CATEGORY);
 
         cy.contains(TEST_CATEGORY).find("svg").click();
         successfulToastShouldNotExist();
-        cats.shouldBeFavorit(TEST_CATEGORY);
+        entities.shouldBeFavorit(TEST_CATEGORY);
       });
 
       it("on a per-group basis", () => {
         const catA = TEST_CATEGORY + "-A";
         const catB = TEST_CATEGORY + "-B";
 
-        cats.add(catA);
-        cats.add(catB);
+        entities.add(catA);
+        entities.add(catB);
 
         cy.contains(catA).find("svg").click();
         successfulToastShwon("Setting default category succeeded!");
-        cats.shouldBeFavorit(catA);
+        entities.shouldBeFavorit(catA);
 
         cy.visit("/groups");
-        groups.add("group2");
+        entities.add("group2");
 
         cy.visit("/categories");
         selectGroup("group2");
 
-        cats.add(catA);
-        cats.add(catB);
+        entities.add(catA);
+        entities.add(catB);
 
         cy.contains(catB).find("svg").click();
         successfulToastShwon("Setting default category succeeded!");
-        cats.shouldBeFavorit(catB);
+        entities.shouldBeFavorit(catB);
 
         selectGroup("just you");
-        cats.shouldBeFavorit(catA);
+        entities.shouldBeFavorit(catA);
       });
     });
   });
