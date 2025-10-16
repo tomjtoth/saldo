3fdf6408d59d5d43ea1f14c12932e54554d15ccb
Tamás Tóth <tomjtoth@gmail.com>
2025-10-16 11:34:58 +0300

    + 2 more e2e tests for receipts

--- cypress/e2e/receipts.cy.ts
+++ cypress/e2e/receipts.cy.ts
@@ -1,13 +1,30 @@
-import { login } from "./session.cy";
+import { login, loginShouldBeVisible } from "./session.cy";
+import { toast } from "./utils.cy";
 
 describe("receipts", () => {
   describe("while logged in", () => {
     beforeEach(() => {
+      cy.request("/api/e2e/receipts");
       login({ page: "/receipts" });
     });
 
     it("can be accessed", () => {
       cy.contains("receipt for group").should("exist");
+    });
+
+    it("can be added", () => {
+      cy.contains("Add new...").click();
+      cy.get("input[placeholder='cost']").type("123");
+      cy.contains("Save & clear").click();
+
+      toast("Submitting new receipt succeeded!");
+    });
+  });
+
+  describe("while *NOT* logged in", () => {
+    it("should redirect to login", () => {
+      cy.visit("/receipts");
+      loginShouldBeVisible();
     });
   });
 });
