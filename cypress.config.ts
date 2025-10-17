9dc992300d94ce0c0aaa1716cbb60dbf9ecf2ac6
Tamás Tóth <tomjtoth@gmail.com>
2025-10-17 14:17:01 +0300

    rm comments from cypress.config.ts

--- cypress.config.ts
+++ cypress.config.ts
@@ -2,8 +2,6 @@ import { defineConfig } from "cypress";
 import codeCoverage from "@cypress/code-coverage/task";
 
 export default defineConfig({
-  // setupNodeEvents can be defined in either
-  // the e2e or component configuration
   e2e: {
     baseUrl: "http://localhost:3000",
     setupNodeEvents(on, config) {
