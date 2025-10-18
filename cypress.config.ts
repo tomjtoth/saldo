f6b586ca25e8d54830f2018e25224fb871e86c90
Tamás Tóth <tomjtoth@gmail.com>
2025-10-18 13:32:49 +0300

    Revert "pointed Cypress to /api/e2e/coverage for data retrieval"
    This reverts commit c26e21a0a2fa8b83427b79796092697b5e2e0bc5.

--- cypress.config.ts
+++ cypress.config.ts
@@ -2,11 +2,6 @@ import { defineConfig } from "cypress";
 import codeCoverage from "@cypress/code-coverage/task";
 
 export default defineConfig({
-  env: {
-    codeCoverage: {
-      url: "http://localhost:3000/api/e2e/coverage",
-    },
-  },
   e2e: {
     baseUrl: "http://localhost:3000",
     setupNodeEvents(on, config) {
