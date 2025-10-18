c26e21a0a2fa8b83427b79796092697b5e2e0bc5
Tamás Tóth <tomjtoth@gmail.com>
2025-10-18 11:32:49 +0300

    pointed Cypress to /api/e2e/coverage for data retrieval

--- cypress.config.ts
+++ cypress.config.ts
@@ -2,6 +2,11 @@ import { defineConfig } from "cypress";
 import codeCoverage from "@cypress/code-coverage/task";
 
 export default defineConfig({
+  env: {
+    codeCoverage: {
+      url: "http://localhost:3000/api/e2e/coverage",
+    },
+  },
   e2e: {
     baseUrl: "http://localhost:3000",
     setupNodeEvents(on, config) {
