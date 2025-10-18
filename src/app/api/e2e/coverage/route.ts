a3d5385e3336c67e6ad548950cb17a46599a9248
Tamás Tóth <tomjtoth@gmail.com>
2025-10-18 13:32:28 +0300

    Revert "impl endpoint /api/e2e/coverage"
    This reverts commit 24d4bb6b50856a8a4f46a8c681beeea37c1f3e30.

--- src/app/api/e2e/coverage/route.ts
+++
@@ -1,9 +0,0 @@
-import protectedRoute from "@/lib/protectedRoute";
-
-export const GET = protectedRoute(
-  { onlyDuringDevelopment: true, requireSession: false },
-  async () =>
-    Response.json({
-      coverage: global.__coverage__ || null,
-    })
-);
