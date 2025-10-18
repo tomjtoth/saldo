62e20e96c64cff2c38a523d0b03ed99ed0e99299
Tamás Tóth <tomjtoth@gmail.com>
2025-10-18 13:41:45 +0300

    `next.config.resolve.fallback.fs = false`

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,14 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: "standalone",
 
+  webpack(config) {
+    config.resolve.fallback = {
+      ...config.resolve.fallback,
+      fs: false,
+    };
+
+    return config;
+  },
   experimental: process.env.INSTRUMENT_E2E
     ? {
         swcPlugins: [
