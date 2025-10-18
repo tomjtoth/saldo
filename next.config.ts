50dafb1748dd0408157c84d19986cb2a771c556c
Tamás Tóth <tomjtoth@gmail.com>
2025-10-18 13:33:47 +0300

    use swc-plugin-coverage-instrument during E2E tests

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,15 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "standalone",
+
+  experimental: process.env.INSTRUMENT_E2E
+    ? {
+        swcPlugins: [
+          // required for e2e lcov retrieval
+          ["swc-plugin-coverage-instrument", {}],
+        ],
+      }
+    : undefined,
 };
 
 export default nextConfig;
