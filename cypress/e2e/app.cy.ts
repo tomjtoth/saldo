5ff6cffb84b8f9827c34cf414dedd66190bca479
Tamás Tóth <tomjtoth@gmail.com>
2025-10-19 12:03:00 +0300

    fix app.cy.ts (failing locally when DB is not empty)

--- cypress/e2e/app.cy.ts
+++ cypress/e2e/app.cy.ts
@@ -1,4 +1,7 @@
+import { cleanup } from "./utils.cy";
+
 it("The import section should be visible", () => {
+  cleanup();
   cy.visit("/");
   cy.get("#import-btn", { timeout: 10000 }).should("exist");
 });
