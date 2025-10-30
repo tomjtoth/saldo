40b49bc93c81ca45837c1b85b0f57036582eb83d
Tamás Tóth <tomjtoth@gmail.com>
2025-10-30 22:43:47 +0200

    same for /balance

--- src/app/balance/page.tsx
+++ src/app/balance/page.tsx
@@ -1,7 +1,7 @@
 import { getBalance } from "@/lib/services/balance";
 
 import wrapPage from "@/lib/wrapPage";
-import CliBalancePage from "@/components/charts/balance";
+import CliBalancePage from "@/app/balance/_components";
 
 export default wrapPage({
   getData: getBalance,
