fe4bbba55758f4ea1f74610e74f5191326c20dc2
Tamás Tóth <tomjtoth@gmail.com>
2025-10-31 13:33:28 +0200

    extended fn wrapService to accept an optional validatorFn

--- src/app/_lib/wrapService.ts
+++ src/app/_lib/wrapService.ts
@@ -1,12 +1,27 @@
 import { currentUser } from "./services/users";
 
-export default function wrapService<O, R>(
-  serverFn: (userId: number, opts: O) => Promise<R>
-) {
-  async function serviceFn(opts: O) {
-    const { id } = await currentUser();
-    return await serverFn(id, opts);
-  }
+type ServerFn<D, R> = (userId: number, data: D) => Promise<R>;
+type ValidatorFn<RawD, D> = (rawData: D | RawD) => D | void;
 
-  return serviceFn;
+function wrapService<RawD, D, R>(
+  serverFn: ServerFn<D, R>,
+  validatorFn: ValidatorFn<RawD, D>
+): (data: RawD) => Promise<R>;
+
+function wrapService<D, R>(serverFn: ServerFn<D, R>): (data: D) => Promise<R>;
+
+function wrapService<RawD, D, R>(
+  serverFn: ServerFn<D, R>,
+  validatorFn?: ValidatorFn<RawD, D>
+) {
+  return async (data: RawD | D): Promise<R> => {
+    const { id } = await currentUser();
+
+    const validatedData = validatorFn
+      ? validatorFn(data) ?? (data as D)
+      : (data as D);
+
+    return serverFn(id, validatedData);
+  };
 }
+export default wrapService;
