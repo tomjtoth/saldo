ee2ff07041b3c28bc8967796166f4ec3325f6c65
Tamás Tóth <tomjtoth@gmail.com>
2025-11-07 13:20:54 +0200

    wrapService to optionally NOT return the result of the serviceFn

--- src/app/_lib/wrapService.ts
+++ src/app/_lib/wrapService.ts
@@ -3,30 +3,50 @@
 import { currentUser } from "../(users)/_lib";
 
 type ServerFn<D, R> = (userId: number, data: D) => Promise<R>;
+
 type ValidatorFn<RawD, D> = (
   rawData: D | RawD,
   userId: number
 ) => (D | void) | Promise<D | void>;
 
+type Options = {
+  returnsResult?: false;
+};
+
 function wrapService<RawD, D, R>(
   serverFn: ServerFn<D, R>,
-  validatorFn: ValidatorFn<RawD, D>
+  validatorFn: ValidatorFn<RawD, D>,
+  options?: Options
 ): (data: RawD) => Promise<R>;
 
-function wrapService<D, R>(serverFn: ServerFn<D, R>): (data: D) => Promise<R>;
+function wrapService<D, R>(
+  serverFn: ServerFn<D, R>,
+  options?: Options
+): (data: D) => Promise<R>;
 
 function wrapService<RawD, D, R>(
   serverFn: ServerFn<D, R>,
-  validatorFn?: ValidatorFn<RawD, D>
+  optsOrFn?: ValidatorFn<RawD, D> | Options,
+  maybeOpts?: Options
 ) {
-  return async (data: RawD | D): Promise<R> => {
+  const opts =
+    typeof maybeOpts === "object"
+      ? maybeOpts
+      : typeof optsOrFn === "object"
+      ? optsOrFn
+      : {};
+
+  const validatorFn = typeof optsOrFn === "function" ? optsOrFn : undefined;
+
+  return async (data: RawD | D): Promise<R | void> => {
     const { id } = await currentUser();
 
     const validatedData = validatorFn
       ? (await validatorFn(data, id)) ?? (data as D)
       : (data as D);
 
-    return serverFn(id, validatedData);
+    const res = serverFn(id, validatedData);
+    if (opts?.returnsResult ?? true) return res;
   };
 }
 
