9083b274940dfa0c13e442484eab7e417e188d51
Tamás Tóth <tomjtoth@gmail.com>
2025-11-08 22:54:49 +0200

    UserMenu revamped

--- src/app/_components/userMenu.tsx
+++ src/app/_components/userMenu.tsx
@@ -1,28 +1,42 @@
 "use client";
 
 import { apiSignOut } from "@/app/api/auth/_lib";
-import { useBodyNodes, useRootDivCx } from "@/app/_lib/hooks";
+import { useBodyNodes, useClientState } from "@/app/_lib/hooks";
 
 import Canceler from "./canceler";
+import UserColorPicker from "./userColorPicker";
+import UserAvatar from "./userAvatar";
 
 export default function UserMenu() {
-  const { user } = useRootDivCx();
+  const user = useClientState().user;
   const nodes = useBodyNodes();
 
   return (
     <Canceler onClick={nodes.pop}>
-      <div className="absolute z-2 top-1/2 left-1/2 -transalte-1/2">
-        <p>
-          Hi, {user?.name ?? "XYou"}!
-          {user?.email && (
-            <>
-              <br />({user.email})
-            </>
-          )}
-        </p>
+      <div
+        className={
+          "absolute z-2 top-1/2 left-1/2 -translate-1/2 " +
+          "p-2 bg-background border rounded flex flex-col gap-2 items-center"
+        }
+      >
+        <div className="flex p-2 gap-2 items-center">
+          <UserAvatar user={user!} className="w-10 h-10" />
+          <div>
+            Hi, {user?.name}!
+            <br />
+            {user?.email}
+          </div>
+        </div>
+
+        <UserColorPicker
+          name="your color in charts"
+          color={user!.color}
+          setLabelColor
+        />
 
         <button
           id="sign-out-button"
+          className="mt-2"
           onClick={() => {
             nodes.pop();
             apiSignOut();
