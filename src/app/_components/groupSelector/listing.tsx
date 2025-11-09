a814b1bf73c655e7b6e1eb19d04a6666c397204b
Tamás Tóth <tomjtoth@gmail.com>
2025-11-09 15:45:59 +0200

    render ViewListing from "/"

--- src/app/_components/groupSelector/listing.tsx
+++ src/app/_components/groupSelector/listing.tsx
@@ -1,5 +1,6 @@
 import { useAppDispatch, useBodyNodes, useClientState } from "@/app/_lib/hooks";
 import { thunks } from "@/app/_lib/reducers";
+
 import ViewListing from "../viewSelector/listing";
 
 export default function GroupListing() {
@@ -7,18 +8,18 @@ export default function GroupListing() {
   const nodes = useBodyNodes();
   const cs = useClientState();
 
-  return (
-    <ul className="text-xl p-2">
+  return cs.groups.length ? (
+    <ul className="p-2">
       {cs.groups.map((group) => {
         const checked = group.id === cs.groupId;
 
         return (
           <li key={group.id}>
-            <label className="cursor-pointer">
+            <label className="text-xl cursor-pointer">
               <input
                 type="radio"
                 radioGroup="a"
-                className="mr-2"
+                className="mr-2 cursor-pointer"
                 checked={checked}
                 onChange={() => {
                   dispatch(thunks.setGroupId(group.id!));
@@ -36,5 +37,10 @@ export default function GroupListing() {
         );
       })}
     </ul>
+  ) : (
+    <div className="p-2">
+      Go to
+      <ViewListing decorate />
+    </div>
   );
 }
