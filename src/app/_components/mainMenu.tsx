fa8bec1fc6c9ecfd5504f2dcc190151840248fc7
Tamás Tóth <tomjtoth@gmail.com>
2025-11-09 11:58:21 +0200

    impl a tabbed MainMenu view handling clicks from Header

---
+++ src/app/_components/mainMenu.tsx
@@ -0,0 +1,61 @@
+"use client";
+
+import { useState } from "react";
+
+import { useBodyNodes, useClientState } from "@/app/_lib/hooks";
+
+import Canceler from "./canceler";
+import UserAvatar from "./userAvatar";
+import UserMenu from "./userMenu";
+import GroupListing from "./groupSelector/listing";
+
+type Tabs = "personal" | "";
+
+export default function MainMenu(opts: { tab?: Tabs } = {}) {
+  const cs = useClientState();
+  const nodes = useBodyNodes();
+
+  const [tab, setTab] = useState<Tabs>(opts.tab ?? "");
+
+  return (
+    <Canceler onClick={nodes.pop}>
+      <div
+        className={
+          "absolute z-2 top-1/2 left-1/2 -translate-1/2 " +
+          "lg:top-20 lg:left-20 lg:translate-0 " +
+          "bg-background border rounded flex flex-col gap-2 items-center"
+        }
+      >
+        <div className="flex place-items-stretch">
+          <div className="grow flex flex-col p-2 gap-4 items-center border-r justify-start">
+            <UserAvatar
+              user={cs.user!}
+              className="w-16 h-16 cursor-pointer"
+              onClick={() => setTab("personal")}
+            />
+
+            <div
+              className="overflow-hidden border rounded-xl cursor-pointer"
+              onClick={() => setTab("")}
+            >
+              <table className="border-collapse [&_td]:p-1 select-none">
+                <tbody>
+                  <tr>
+                    <td className="border-r">🛍️</td>
+                    <td className="border-b">🧾</td>
+                  </tr>
+                  <tr>
+                    <td className="border-t">📊</td>
+                    <td className="border-l">📈</td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
+          </div>
+
+          {tab === "personal" ? <UserMenu /> : <GroupListing />}
+        </div>
+      </div>
+    </Canceler>
+  );
+}
