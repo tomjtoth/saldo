38cac63536858ce3f67aa289c1337e137a9e18c9
Tamás Tóth <tomjtoth@gmail.com>
2025-11-09 15:13:11 +0200

    ViewListing can be decorated

--- src/app/_components/viewSelector/listing.tsx
+++ src/app/_components/viewSelector/listing.tsx
@@ -9,14 +9,20 @@ const LINKS = [
 
 export const hrefToLabel = (href: string) => href.replaceAll(/\W+/g, "");
 
-export default function ViewListing({ prefix = "" }: { prefix?: string }) {
+export default function ViewListing({
+  prefix = "",
+  decorate,
+}: {
+  prefix?: string;
+  decorate?: true;
+}) {
   const lastIdx = LINKS.length - 1;
 
   return (
     <ul>
       {LINKS.map((a, idx) => (
         <li key={a.href}>
-          {idx === lastIdx ? "└ " : "├ "}
+          {decorate && (idx === lastIdx ? "└ " : "├ ")}
           <Link href={prefix + a.href}>{hrefToLabel(a.href)}</Link>
         </li>
       ))}
