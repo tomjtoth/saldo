ffab829f7da4a3bfda3cdf706086f5012e6b15a0
Tamás Tóth <tomjtoth@gmail.com>
2025-11-09 23:05:01 +0200

    MainMenu styling

--- src/app/_components/mainMenu.tsx
+++ src/app/_components/mainMenu.tsx
@@ -22,40 +22,38 @@ export default function MainMenu(opts: { tab?: Tabs } = {}) {
     <Canceler onClick={nodes.pop}>
       <div
         className={
-          "absolute z-2 top-1/2 left-1/2 -translate-1/2 " +
-          "lg:top-20 lg:left-20 lg:translate-0 " +
-          "bg-background border rounded flex flex-col gap-2 items-center"
+          "absolute block z-2 max-w-9/10 max-h-9/10 " +
+          "top-1/2 lg:top-20 left-1/2 lg:left-20 -translate-1/2 lg:translate-0 " +
+          "bg-background border rounded flex gap-2 items-stretch"
         }
       >
-        <div className="flex place-items-stretch">
-          <div className="grow flex flex-col p-2 gap-4 items-center border-r justify-start">
-            <UserAvatar
-              user={cs.user!}
-              className="w-16 h-16 cursor-pointer"
-              onClick={() => setTab("personal")}
-            />
+        <div className="min-w-max flex flex-col p-2 gap-4 items-center border-r">
+          <UserAvatar
+            user={cs.user!}
+            className="w-16 h-16 cursor-pointer"
+            onClick={() => setTab("personal")}
+          />
 
-            <div
-              className="overflow-hidden border rounded-xl cursor-pointer"
-              onClick={() => setTab("")}
-            >
-              <table className="border-collapse [&_td]:p-1 select-none">
-                <tbody>
-                  <tr>
-                    <td className="border-r">{LINKS[0].emoji}</td>
-                    <td className="border-b">{LINKS[1].emoji}</td>
-                  </tr>
-                  <tr>
-                    <td className="border-t">{LINKS[2].emoji}</td>
-                    <td className="border-l">{LINKS[3].emoji}</td>
-                  </tr>
-                </tbody>
-              </table>
-            </div>
+          <div
+            className="overflow-hidden border rounded-xl cursor-pointer"
+            onClick={() => setTab("")}
+          >
+            <table className="border-collapse [&_td]:p-1 select-none">
+              <tbody>
+                <tr>
+                  <td className="border-r">{LINKS[0].emoji}</td>
+                  <td className="border-b">{LINKS[1].emoji}</td>
+                </tr>
+                <tr>
+                  <td className="border-t">{LINKS[2].emoji}</td>
+                  <td className="border-l">{LINKS[3].emoji}</td>
+                </tr>
+              </tbody>
+            </table>
           </div>
-
-          {tab === "personal" ? <UserMenu /> : <GroupListing />}
         </div>
+
+        {tab === "personal" ? <UserMenu /> : <GroupListing />}
       </div>
     </Canceler>
   );
