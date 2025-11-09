bfc4d0ab43da8d8f1ffba893c1f22fce1f838ece
Tamás Tóth <tomjtoth@gmail.com>
2025-11-09 23:40:46 +0200

    default to saldo.db as DB name

--- src/app/_lib/db/instance.ts
+++ src/app/_lib/db/instance.ts
@@ -5,7 +5,7 @@ import * as schema from "@/app/_lib/db/schema";
 export const db = drizzle({
   connection:
     process.env.DATABASE_URL ??
-    (process.env.NODE_ENV === "development" ? "file:data/dev.db" : ":memory:"),
+    (process.env.NODE_ENV === "development" ? "file:data/dev.db" : "saldo.db"),
   schema,
   casing: "snake_case",
   logger: true,
