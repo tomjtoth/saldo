8fc8772ee129b75005942a2ba300db7d83a45a3c
Tamás Tóth <tomjtoth@gmail.com>
2025-11-11 19:35:42 +0200

    baseline check against cy.populateDb()

--- cypress/e2e/app.cy.ts
+++ cypress/e2e/app.cy.ts
@@ -3,3 +3,10 @@ it("The import section should be visible", () => {
   cy.visit("/");
   cy.get("#import-btn", { timeout: 10000 }).should("exist");
 });
+
+it("populating DB works as expected", () => {
+  cy.populateDb();
+  cy.readDb().then(({ baseline, response }) => {
+    expect(baseline).to.deep.eq(response);
+  });
+});
