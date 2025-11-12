8c77f8fba73af0fe1f455fbdb2032662604b1f55
Tamás Tóth <tomjtoth@gmail.com>
2025-11-12 19:38:47 +0200

    /api/e2e/db to populate groups, categories, receipts, balance, consumption on a per-user basis

--- src/app/api/e2e/db/route.ts
+++ src/app/api/e2e/db/route.ts
@@ -2,6 +2,11 @@ import { sql } from "drizzle-orm";
 
 import { db, Schema, SchemaTables } from "@/app/_lib/db";
 import * as schema from "@/app/_lib/db/schema";
+import { svcGetGroups } from "@/app/groups/_lib";
+import { svcGetCategories } from "@/app/categories/_lib";
+import { svcGetReceipts } from "@/app/receipts/_lib";
+import { svcGetBalance } from "@/app/(charts)/balance/_lib";
+import { svcGetConsumption } from "@/app/(charts)/consumption/_lib";
 import wrapRoute from "@/app/_lib/wrapRoute";
 
 export const GET = wrapRoute(
@@ -9,7 +14,17 @@ export const GET = wrapRoute(
   async () => {
     const res: {
       [K in Partial<keyof SchemaTables>]?: Schema[K]["$inferSelect"][];
-    } = {};
+    } & {
+      perUser: {
+        [userId: number]: {
+          groups: Awaited<ReturnType<typeof svcGetGroups>>;
+          categories: Awaited<ReturnType<typeof svcGetCategories>>;
+          receipts: Awaited<ReturnType<typeof svcGetReceipts>>;
+          balance: Awaited<ReturnType<typeof svcGetBalance>>;
+          consumption: Awaited<ReturnType<typeof svcGetConsumption>>;
+        };
+      };
+    } = { perUser: {} };
 
     res.metadata = await db.all(sql`SELECT * FROM metadata`);
     res.archives = await db.all(sql`
@@ -35,6 +50,16 @@ export const GET = wrapRoute(
     ] as (keyof SchemaTables)[]) {
       // store selected rows per table into result object
       res[tbl] = (await db.select().from(schema[tbl])) as any;
+    }
+
+    for (const user of res.users!) {
+      res.perUser[user.id] = {
+        groups: await svcGetGroups(user.id),
+        categories: await svcGetCategories(user.id),
+        receipts: await svcGetReceipts(user.id),
+        balance: await svcGetBalance(user.id),
+        consumption: await svcGetConsumption(user.id),
+      };
     }
 
     return res;
