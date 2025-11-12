5b88b3ef127ad3785552774e95262aebd4fd7c59
Tamás Tóth <tomjtoth@gmail.com>
2025-11-12 12:55:31 +0200

    query table names in snake_case in /api/e2e/db

--- src/app/api/e2e/db/route.ts
+++ src/app/api/e2e/db/route.ts
@@ -1,5 +1,6 @@
 import { sql } from "drizzle-orm";
 
+import { camelToSnakeCase } from "@/app/_lib/utils";
 import { db, SchemaTables } from "@/app/_lib/db";
 import wrapRoute from "@/app/_lib/wrapRoute";
 
@@ -23,7 +24,9 @@ export const GET = wrapRoute(
       "items",
       "itemShares",
     ] as (keyof SchemaTables)[]) {
-      res[tbl] = await db.all(sql`SELECT * FROM ${sql.raw(tbl)}`);
+      res[tbl] = await db.all(
+        sql`SELECT * FROM ${sql.raw(camelToSnakeCase(tbl))}`
+      );
     }
 
     return res;
