aaaec12f954befb5f485e85656ca3baab21bc79a
Tamás Tóth <tomjtoth@gmail.com>
2025-11-13 17:47:04 +0200

    tied return values of svc{Add,Mod,Get}Categories together without casting

--- src/app/categories/_lib/index.ts
+++ src/app/categories/_lib/index.ts
@@ -7,29 +7,39 @@ import {
   db,
   getArchivePopulator,
   isActive,
-  TCategory,
-  TGroup,
   modEntity,
+  DbCategory,
 } from "@/app/_lib/db";
 import { categories, groups, memberships } from "@/app/_lib/db/schema";
-import { currentUser } from "@/app/(users)/_lib";
+import { currentUser, User } from "@/app/(users)/_lib";
 import { svcModMembership } from "@/app/(memberships)/_lib";
 import {
   err,
   has3ConsecutiveLetters,
   nullEmptyStrings,
-  nulledEmptyStrings,
   sortByName,
 } from "@/app/_lib/utils";
 
-type RequiredCategoryFields = Required<
-  Pick<TCategory, "groupId" | "name" | "description">
->;
+const WITH_CATEGORIES = {
+  with: {
+    revision: {
+      columns: {
+        createdAt: true,
+      },
+      with: {
+        createdBy: { columns: { name: true } },
+      },
+    },
+  },
+} as const;
 
-export async function apiAddCategory(uncheckedData: RequiredCategoryFields) {
-  const user = await currentUser();
+type CategoryAdder = Pick<DbCategory, "groupId" | "name" | "description">;
 
-  const { groupId, name, description } = uncheckedData;
+export async function apiAddCategory({
+  groupId,
+  name,
+  description,
+}: CategoryAdder) {
   const typeDescr = typeof description;
 
   if (
@@ -45,13 +55,17 @@ export async function apiAddCategory(uncheckedData: RequiredCategoryFields) {
 
   nullEmptyStrings(data);
 
+  const user = await currentUser();
+
   return await svcAddCategory(user.id, data);
 }
 
+export type Category = Awaited<ReturnType<typeof svcModCategory>>;
+
 export async function svcAddCategory(
   revisedBy: number,
-  data: RequiredCategoryFields
-) {
+  data: CategoryAdder
+): Promise<Category> {
   return await atomic(
     { operation: "Creating category", revisedBy },
     async (tx, revisionId) => {
@@ -64,26 +78,17 @@ export async function svcAddCategory(
         .returning({ id: categories.id });
 
       const res = await tx.query.categories.findFirst({
-        with: {
-          revision: {
-            columns: {
-              createdAt: true,
-            },
-            with: {
-              createdBy: { columns: { name: true } },
-            },
-          },
-        },
+        ...WITH_CATEGORIES,
         where: eq(categories.id, id),
       });
 
-      return res as TCategory;
+      return { ...res!, archives: [] };
     }
   );
 }
 
-type CategoryModifier = Required<Pick<TCategory, "id">> &
-  Pick<TCategory, "name" | "description" | "flags">;
+type CategoryModifier = Pick<DbCategory, "id"> &
+  Partial<Pick<DbCategory, "name" | "description" | "flags">>;
 
 export async function apiModCategory({
   id,
@@ -99,12 +104,14 @@ export async function apiModCategory({
   )
     err();
 
-  const data = nulledEmptyStrings({
+  const data = {
     id,
     name,
     description,
     flags,
-  });
+  };
+
+  nullEmptyStrings(data);
 
   const user = await currentUser();
 
@@ -114,48 +121,44 @@ export async function apiModCategory({
 }
 
 export async function svcModCategory(
-  revisedBy: number,
+  revisedBy: User["id"],
   { id, ...modifier }: CategoryModifier
-) {
+): Promise<
+  Awaited<ReturnType<typeof svcGetCategories>>[number]["categories"][number]
+> {
   return await atomic(
     { operation: "Updating category", revisedBy },
     async (tx, revisionId) => {
-      const cat = (await tx.query.categories.findFirst({
+      const cat = await tx.query.categories.findFirst({
         where: eq(categories.id, id),
-      }))!;
+      });
 
-      const res = (await modEntity(cat, modifier, {
+      if (!cat) err(404);
+
+      await modEntity(cat, modifier, {
         tx,
         tableName: "categories",
         revisionId,
         primaryKeys: { id: true },
+      });
 
-        returns: {
-          with: {
-            revision: {
-              columns: {
-                createdAt: true,
-              },
-              with: { createdBy: { columns: { name: true } } },
-            },
-          },
-        },
-      })) as TCategory;
+      // TODO: merge this call into modEntity and
+      // get the ReturnType according to the args passed
+      const res = await tx.query.categories.findFirst({
+        ...WITH_CATEGORIES,
+        where: eq(categories.id, cat.id),
+      });
 
-      const populateArchives = await getArchivePopulator<TCategory>(
-        "categories",
-        "id",
-        { tx }
-      );
+      const withArchives = await getArchivePopulator("categories", "id", {
+        tx,
+      });
 
-      populateArchives([res]);
-
-      return res;
+      return withArchives([res!])[0];
     }
   );
 }
 
-export async function apiSetDefaultCategory(categoryId: number) {
+export async function apiSetDefaultCategory(categoryId: DbCategory["id"]) {
   if (typeof categoryId !== "number") err();
 
   const { id: userId } = await currentUser();
@@ -198,7 +201,7 @@ async function userHasAccessToCategory(userId: number, catId: number) {
 }
 
 export async function svcGetCategories(userId: number) {
-  const res: TGroup[] = await db.query.groups.findMany({
+  const res = await db.query.groups.findMany({
     columns: {
       id: true,
       name: true,
@@ -213,14 +216,7 @@ export async function svcGetCategories(userId: number) {
         columns: { createdAt: true },
         with: { createdBy: { columns: { id: true, name: true } } },
       },
-      categories: {
-        with: {
-          revision: {
-            columns: { createdAt: true },
-            with: { createdBy: { columns: { name: true } } },
-          },
-        },
-      },
+      categories: WITH_CATEGORIES,
     },
 
     where: and(
@@ -240,16 +236,12 @@ export async function svcGetCategories(userId: number) {
     ),
   });
 
-  const populateArchives = await getArchivePopulator<TCategory>(
-    "categories",
-    "id"
-  );
+  const withArchives = await getArchivePopulator("categories", "id");
 
   res.sort(sortByName);
-  res.forEach((g) => {
-    g.categories!.sort(sortByName);
-    populateArchives(g.categories!);
-  });
 
-  return res;
+  return res.map((group) => ({
+    ...group,
+    categories: withArchives(group.categories.toSorted(sortByName)),
+  }));
 }
