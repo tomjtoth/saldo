2512d0760a63b85a3241962d7337eee3f90dabfb
Tamás Tóth <tomjtoth@gmail.com>
2025-11-16 23:36:28 +0200

    adjusted fn populateReceiptArchivesRecursively to new archivePopulators

--- src/app/receipts/_lib/common.ts
+++ src/app/receipts/_lib/common.ts
@@ -2,7 +2,10 @@ import { eq, sql, desc } from "drizzle-orm";
 
 import { receipts } from "@/app/_lib/db/schema";
 import { QueryParamsOf, DbReceipt, DrizzleTx } from "@/app/_lib/db/types";
-import { getArchivePopulator } from "@/app/_lib/db/archives";
+import {
+  ArchivePopulatorFn,
+  getArchivePopulator,
+} from "@/app/_lib/db/archives";
 import { db } from "@/app/_lib/db";
 import { SELECT_RECEIPTS } from "@/app/_lib";
 
@@ -22,58 +25,71 @@ const getMany = (receiptId: DbReceipt["id"], tx?: DrizzleTx) =>
     where: eq(receipts.id, receiptId),
   });
 
-type ManyFromDb = Awaited<ReturnType<typeof getMany>>;
+export type ReceiptsFromDb = Awaited<ReturnType<typeof getMany>>;
 
 export type Receipt = Awaited<ReturnType<typeof populateRecursively>>[number];
 export type Item = Receipt["items"][number];
 export type ItemShare = Item["itemShares"][number];
+
+export function populateReceiptArchivesRecursively(
+  receipts: ReceiptsFromDb,
+  populator: ArchivePopulatorFn
+): Promise<Receipt[]>;
+
+export async function populateReceiptArchivesRecursively(
+  receipts: ReceiptsFromDb,
+  tx?: DrizzleTx
+): Promise<Receipt[]>;
 
 export async function populateReceiptArchivesRecursively(
   ...args: Parameters<typeof getMany>
 ): Promise<Receipt>;
 
 export async function populateReceiptArchivesRecursively(
-  receipts: ManyFromDb,
-  tx?: DrizzleTx
-): Promise<Receipt[]>;
-
-export async function populateReceiptArchivesRecursively(
-  idOrArr: DbReceipt["id"] | ManyFromDb,
-  tx?: DrizzleTx
+  idOrArr: DbReceipt["id"] | ReceiptsFromDb,
+  txOrPopulator?: DrizzleTx | ArchivePopulatorFn
 ) {
   const single = typeof idOrArr === "number";
 
-  const receipts = single ? await getMany(idOrArr, tx) : idOrArr;
-  const populated = await populateRecursively(receipts, tx);
+  const receipts = single
+    ? await getMany(
+        idOrArr,
+        typeof txOrPopulator === "function" ? undefined : txOrPopulator
+      )
+    : idOrArr;
+
+  const populated = await populateRecursively(receipts, txOrPopulator);
 
   return single ? populated[0] : populated;
 }
 
-async function populateRecursively(arr: ManyFromDb, tx?: DrizzleTx) {
-  const receiptWithArchives = await getArchivePopulator("receipts", "id", {
-    tx,
-  });
+async function populateRecursively(
+  arr: ReceiptsFromDb,
+  txOrPopulator?: DrizzleTx | ArchivePopulatorFn
+) {
+  const populator =
+    typeof txOrPopulator === "function"
+      ? txOrPopulator
+      : await getArchivePopulator(txOrPopulator);
 
-  const itemsWithArchives = await getArchivePopulator("items", "id", {
-    tx,
-  });
-
-  const itemSharesWithArchives = await getArchivePopulator(
-    "itemShares",
-    "itemId",
-    {
-      pk2: "userId",
-      tx,
-    }
-  );
-
-  return receiptWithArchives(
+  return populator(
+    "receipts",
     arr.map((receipt) => ({
       ...receipt,
-      items: itemsWithArchives(
+      items: populator(
+        "items",
         receipt.items.map(({ itemShares, ...item }) => ({
           ...item,
-          itemShares: itemSharesWithArchives(itemShares),
+          itemShares: populator(
+            {
+              table: "itemShares",
+              primaryKeys: {
+                userId: true,
+                itemId: true,
+              },
+            },
+            itemShares
+          ),
         }))
       ),
     }))
