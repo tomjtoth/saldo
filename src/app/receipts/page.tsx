240194d80f2adfc71956a8b36d55f3ef8e7918cb
Tamás Tóth <tomjtoth@gmail.com>
2025-11-19 00:23:25 +0200

    receipts/page to call svcGetGroups with updated signature

--- src/app/receipts/page.tsx
+++ src/app/receipts/page.tsx
@@ -4,7 +4,7 @@ import wrapPage from "@/app/_lib/wrapPage";
 import CliReceiptsPage from "./_components";
 
 export default wrapPage({
-  getData: (userId) => svcGetGroups(userId, { view: "receipts" }),
+  getData: (userId) => svcGetGroups(userId, { extras: { receipts: true } }),
   children: <CliReceiptsPage />,
   rewritePath: "/receipts",
 });
