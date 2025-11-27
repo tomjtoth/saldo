57228d0caaf8344a02db04bdb11ca5ab863e469a
Tamás Tóth <tomjtoth@gmail.com>
2025-11-27 16:34:33 +0200

    receipts client-side adjusted

--- src/app/receipts/_lib/hook.ts
+++ src/app/receipts/_lib/hook.ts
@@ -1,75 +1,56 @@
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 import { v4 as uuid } from "uuid";
 
 import { useAppDispatch, useClientState, useRootDivCx } from "@/app/_lib/hooks";
 import { thunks } from "@/app/_lib/reducers";
-import { apiGetReceipts } from "../_lib";
 
 const INFINITE_SCROLL = `INFINITE_SCROLL-${uuid()}`;
 
 export default function useInfiniteScroll() {
   const cs = useClientState();
-  const { addOnScroll, rmOnScroll, rootDivRef } = useRootDivCx();
+  const cx = useRootDivCx();
   const dispatch = useAppDispatch();
 
-  const [fetching, setFetching] = useState(false);
-  const [debounce, setDebounce] = useState(0);
-  const [hasMore, setHasMore] = useState<{ [groupId: number]: boolean }>({});
-
   useEffect(() => {
-    const rootDivH = rootDivRef?.current?.scrollHeight ?? 0;
+    const rootDivH = cx.rootDivRef?.current?.scrollHeight ?? 0;
 
     if (
       !!cs.groupId &&
-      (hasMore[cs.groupId] ?? true) &&
-      !fetching &&
-      (rootDivH < window.innerHeight || debounce === 1)
+      (cs.group?.hasMoreToLoad ?? true) &&
+      !cs.group?.fetchingReceipts &&
+      (rootDivH < window.innerHeight ||
+        cs.group?.debounceReceiptsFetching === 1)
     ) {
-      // eslint-disable-next-line react-hooks/set-state-in-effect
-      setFetching(true);
-      const fetchedGroupId = cs.groupId;
-
-      apiGetReceipts(
-        cs.groups.flatMap((grp) => grp.receipts?.map((r) => r.id!) ?? [])
-      ).then((groups) => {
-        let updating = false;
-        const limits = { ...hasMore };
-
-        if (groups.length === 0) {
-          updating = true;
-          limits[fetchedGroupId] = false;
-        }
-
-        const storing = groups
-          .map((group) => {
-            const len = group.receipts?.length ?? 0;
-            if (len < 50) {
-              updating = true;
-              limits[group.id!] = false;
-            }
-
-            return len > 0 ? true : false;
-          })
-          .some(Boolean);
-
-        if (storing) dispatch(thunks.addFetchedReceipts(groups));
-
-        if (updating) setHasMore(limits);
-        setFetching(false);
-        setDebounce(0);
-      });
+      dispatch(
+        thunks.fetchReceipts(
+          cs.groupId!,
+          cs.group!.receipts.map(({ id }) => id)
+        )
+      );
     }
-  }, [fetching, debounce]);
+
+    return () => {
+      // componentUnmounted = true;
+    };
+  }, [
+    cs.group?.fetchingReceipts,
+    cs.group?.debounceReceiptsFetching,
+    cs.groupId,
+  ]);
 
   useEffect(() => {
-    addOnScroll(INFINITE_SCROLL, (ev) => {
+    cx.addOnScroll(INFINITE_SCROLL, (ev) => {
       const triggered =
-        window.innerHeight + ev.currentTarget.scrollTop + 1500 >
-        ev.currentTarget.scrollHeight;
+        (window.innerHeight + ev.currentTarget.scrollTop) /
+          ev.currentTarget.scrollHeight >
+        0.8;
 
-      if (triggered) setDebounce(debounce + 1);
+      if (triggered && !cs.group?.fetchingReceipts) {
+        dispatch(thunks.tryFetchingReceipts());
+        console.debug("scroll triggered");
+      }
     });
 
-    return () => rmOnScroll(INFINITE_SCROLL);
+    return () => cx.rmOnScroll(INFINITE_SCROLL);
   }, []);
 }
--- src/app/receipts/_lib/reducers/slices.ts
+++ src/app/receipts/_lib/reducers/slices.ts
@@ -1,8 +1,9 @@
 import { PayloadAction } from "@reduxjs/toolkit";
 
-import { TReceipt, TGroup } from "@/app/_lib/db";
+import { Item, Receipt } from "../populateRecursively";
+import { Group } from "@/app/groups/_lib";
 import { deepClone } from "@/app/_lib/utils";
-import { CombinedState as CS, TCliItem } from "@/app/_lib/reducers/types";
+import { CombinedState as CS } from "@/app/_lib/reducers/types";
 import {
   addItem,
   getActiveGroup,
@@ -12,48 +13,51 @@ import {
   sortReceipts,
 } from "./helpers";
 
+export type ItemModifier = Pick<Item, "id"> &
+  Partial<Pick<Item, "categoryId" | "cost" | "notes" | "itemShares">>;
+
 export const sliceReceipts = {
-  setPaidOn(rs: CS, { payload }: PayloadAction<string>) {
+  setPaidOn(rs: CS, { payload }: PayloadAction<Receipt["paidOn"]>) {
     const receipt = getActiveReceipt(rs);
     receipt.paidOn = payload;
   },
 
-  setPaidBy(rs: CS, { payload }: PayloadAction<number>) {
+  setPaidBy(rs: CS, { payload }: PayloadAction<Receipt["paidById"]>) {
     const receipt = getActiveReceipt(rs);
     const users = getActiveUsers(rs);
-    const user = users?.find((u) => u.id === payload);
+    const user = users.find((u) => u.id === payload)!;
 
     receipt.paidBy = user;
     receipt.paidById = payload;
   },
 
-  addRow(rs: CS, { payload }: PayloadAction<number | undefined>) {
+  addRow(rs: CS, { payload }: PayloadAction<Item["id"] | undefined>) {
     const receipt = getActiveReceipt(rs);
 
     if (payload !== undefined) {
-      const idx = receipt.items!.findIndex((i) => i.id === payload);
+      const idx = receipt.items.findIndex((i) => i.id === payload);
       receipt.focusedIdx = idx + 1;
-      receipt.items!.splice(
+      receipt.items.splice(
         idx + 1,
         0,
         // inherit categoryId from the row above
-        addItem(receipt.items![idx].categoryId!)
+        addItem(receipt.items[idx].categoryId)
       );
     } else {
       const group = getActiveGroup(rs);
 
       if (group) {
         const defCat = getDefaultCategory(rs, group.id);
-        receipt.items!.push(addItem(defCat));
+        receipt.items.push(addItem(defCat));
       }
     }
   },
 
-  rmRow(rs: CS, { payload }: PayloadAction<number>) {
+  rmRow(rs: CS, { payload }: PayloadAction<Item["id"]>) {
     const receipt = getActiveReceipt(rs);
-    const idx = receipt.items!.findIndex((i) => i.id === payload);
+    const idx = receipt.items.findIndex((i) => i.id === payload);
 
-    receipt.items!.splice(idx, 1);
+    receipt.items.splice(idx, 1);
   },
 
   setFocusedRow(rs: CS, { payload }: PayloadAction<number>) {
@@ -61,53 +65,67 @@ export const sliceReceipts = {
     receipt.focusedIdx = payload;
   },
 
-  modItem(rs: CS, { payload }: PayloadAction<TCliItem>) {
+  modItem(rs: CS, { payload }: PayloadAction<ItemModifier>) {
     const receipt = getActiveReceipt(rs);
 
-    const item = receipt.items!.find((i) => i.id === payload.id)!;
+    const item = receipt.items.find((i) => i.id === payload.id)!;
     if (payload.categoryId !== undefined) item.categoryId = payload.categoryId;
     if (payload.cost !== undefined) item.cost = payload.cost;
     if (payload.notes !== undefined) item.notes = payload.notes;
     if (payload.itemShares !== undefined) item.itemShares = payload.itemShares;
   },
 
-  modReceipt(rs: CS, { payload }: PayloadAction<TReceipt>) {
+  modReceipt(rs: CS, { payload }: PayloadAction<Receipt>) {
     const group = getActiveGroup(rs, payload.groupId);
 
-    const insertAt = group.receipts!.findIndex((r) => r.id === payload.id);
+    const insertAt = group.receipts.findIndex((r) => r.id === payload.id);
 
-    group.receipts!.splice(insertAt, 1, payload);
+    group.receipts.splice(insertAt, 1, payload);
 
     delete group["activeReceipt"];
   },
 
-  addReceipt(rs: CS, { payload }: PayloadAction<TReceipt>) {
+  addReceipt(rs: CS, { payload }: PayloadAction<Receipt>) {
     const group = getActiveGroup(rs, payload.groupId);
 
-    const insertAt = group.receipts!.findIndex(
-      (r) => r.paidOn! < payload.paidOn!
-    );
+    const insertAt = group.receipts.findIndex((r) => r.paidOn < payload.paidOn);
 
-    group.receipts!.splice(insertAt < 0 ? 0 : insertAt, 0, payload);
+    group.receipts.splice(insertAt < 0 ? 0 : insertAt, 0, payload);
 
     delete group["activeReceipt"];
   },
 
-  addFetchedReceipts(rs: CS, { payload }: PayloadAction<TGroup[]>) {
-    payload.forEach((grp) => {
-      rs.groups
-        .find((group) => group.id === grp.id)
-        ?.receipts?.push(...grp.receipts!);
-    });
+  addFetchedReceipts(
+    rs: CS,
+    { payload }: PayloadAction<{ groupId: Group["id"]; receipts: Receipt[] }>
+  ) {
+    const group = getActiveGroup(rs, payload.groupId);
 
-    sortReceipts(rs.groups);
+    if (payload.receipts.length) {
+      group.receipts.push(...payload.receipts);
+      group.hasMoreToLoad = true;
+      group.fetchingReceipts = false;
+      group.debounceReceiptsFetching = 0;
+      sortReceipts(rs.groups);
+    } else {
+      group.hasMoreToLoad = false;
+    }
   },
 
-  setActiveReceipt(rs: CS, { payload }: PayloadAction<number | undefined>) {
+  tryFetchingReceipts(rs: CS) {
+    const group = getActiveGroup(rs);
+
+    group.debounceReceiptsFetching = (group.debounceReceiptsFetching ?? 0) + 1;
+  },
+
+  setActiveReceipt(
+    rs: CS,
+    { payload }: PayloadAction<Receipt["id"] | undefined>
+  ) {
     const group = getActiveGroup(rs);
 
     if (typeof payload === "number") {
-      const activeReceipt = group.receipts?.find((rcpt) => rcpt.id === payload);
+      const activeReceipt = group.receipts.find((rcpt) => rcpt.id === payload);
 
       const detachedClone = deepClone(activeReceipt);
       group.activeReceipt = detachedClone;
