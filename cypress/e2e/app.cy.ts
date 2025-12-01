a3b892a07e972bd34e8350737ed666e80d5d9a84
Tamás Tóth <tomjtoth@gmail.com>
2025-12-02 00:13:59 +0200

    baseline check agains consumption and balance data

--- cypress/e2e/app.cy.ts
+++ cypress/e2e/app.cy.ts
@@ -4,11 +4,11 @@ it("The import section should be visible", () => {
   cy.get("#import-btn", { timeout: 10000 }).should("exist");
 });
 
-it.only("populating DB works as expected", () => {
+it("populating DB works as expected", () => {
   cy.populateDb();
   cy.readDb().then(({ baseline, response }) => {
-    for (const iUser in baseline) {
-      const key = iUser as keyof typeof baseline;
+    for (const userId in baseline) {
+      const key = userId as keyof typeof baseline;
 
       baseline[key].forEach((bGroup, iGroup) => {
         const rGroup = response[key][iGroup];
@@ -40,5 +40,22 @@ it.only("populating DB works as expected", () => {
     }
 
     expect(baseline).to.deep.eq(response);
+
+    // consumption should look the same for each member
+    expect(baseline[1][0].consumption).to.deep.eq(baseline[2][0].consumption);
+    expect(baseline[1][0].consumption).to.deep.eq(baseline[3][0].consumption);
+    expect(baseline[2][1].consumption).to.deep.eq(baseline[3][1].consumption);
+
+    // balance should look the same for each member except
+    // user1 sees only group1, and balance.minMaxes is a buffer
+    // that holds unified data of every single group a user has access to
+    const u1g1withExtendedMinMaxes = {
+      ...baseline[1][0].balance,
+      minMaxes: baseline[2][0].balance.minMaxes,
+    };
+
+    expect(u1g1withExtendedMinMaxes).to.deep.eq(baseline[2][0].balance);
+    expect(u1g1withExtendedMinMaxes).to.deep.eq(baseline[3][0].balance);
+    expect(baseline[2][1].balance).to.deep.eq(baseline[3][1].balance);
   });
 });
