b1743ff87e2664efbfae21233c5cb62c1aa156a6
Tamás Tóth <tomjtoth@gmail.com>
2025-12-10 19:13:07 +0200

    adjusted svcGetCategoryViaUserAccess

--- src/app/categories/_lib/access.ts
+++ src/app/categories/_lib/access.ts
@@ -1,10 +1,7 @@
 "use server";
 
-import { and, eq, exists, sql } from "drizzle-orm";
-
 import { db, isActive } from "@/app/_lib/db";
 import { err, ErrOpts } from "@/app/_lib/utils";
-import { categories, groups, memberships } from "@/app/_lib/db/schema";
 import { User } from "@/app/(users)/_lib";
 import { Category } from "./getCategories";
 
@@ -14,23 +11,18 @@ export async function svcGetCategoryViaUserAccess(
   opts?: Pick<ErrOpts, "info" | "args">
 ) {
   const category = await db.query.categories.findFirst({
-    where: and(
-      eq(categories.id, categoryId),
-      exists(
-        db
-          .select({ x: sql`1` })
-          .from(memberships)
-          .innerJoin(groups, eq(memberships.groupId, groups.id))
-          .where(
-            and(
-              eq(memberships.groupId, categories.groupId),
-              eq(memberships.userId, userId),
-              isActive(memberships),
-              isActive(groups)
-            )
-          )
-      )
-    ),
+    where: {
+      id: categoryId,
+
+      group: {
+        RAW: isActive,
+
+        memberships: {
+          RAW: isActive,
+          userId,
+        },
+      },
+    },
   });
 
   if (!category)
