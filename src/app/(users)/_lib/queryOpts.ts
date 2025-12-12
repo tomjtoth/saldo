0bcedc3042be10bf24777d86e7db935601d8a403
Tamás Tóth <tomjtoth@gmail.com>
2025-12-12 15:43:26 +0200

    refactored (users)/_lib

---
+++ src/app/(users)/_lib/queryOpts.ts
@@ -0,0 +1,20 @@
+import { sql } from "drizzle-orm";
+
+import { QueryParamsOf } from "@/app/_lib/db";
+
+export const USERS_EXTRAS = {
+  extras: {
+    color: (users) => sql<string>`printf(
+      '#%06x', 
+      coalesce(
+        (
+          SELECT color FROM chart_colors
+          WHERE user_id = ${users.id}
+          AND group_id IS NULL
+          AND member_id IS NULL
+        ),
+        abs(random()) % 0x1000000
+      )
+    )`,
+  },
+} as const satisfies QueryParamsOf<"users">;
