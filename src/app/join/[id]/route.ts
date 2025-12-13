41c6e0f1ffa1b4d712b51e29349bcadb28a6d9b8
Tamás Tóth <tomjtoth@gmail.com>
2025-12-13 20:32:58 +0200

    missing from fa15736675d0ac1cbf9af4a2d555851091bc077d

--- src/app/join/[id]/route.ts
+++ src/app/join/[id]/route.ts
@@ -3,11 +3,7 @@ import { redirect } from "next/navigation";
 import { joinGroup } from "@/app/groups/_lib";
 import wrapRoute from "@/app/_lib/wrapRoute";
 
-export const GET = wrapRoute<{ id: string }>(
-  { redirectAs: ({ params }) => `/join/${params.id}` },
-
-  async ({ params, user }) => {
-    await joinGroup(params.id, user.id);
-    redirect("/groups");
-  }
-);
+export const GET = wrapRoute<{ id: string }>(async ({ params, user }) => {
+  await joinGroup(params.id, user.id);
+  redirect("/groups");
+});
