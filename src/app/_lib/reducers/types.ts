ef98469d3cfbd322213836b55085239fb06c447c
Tamás Tóth <tomjtoth@gmail.com>
2025-12-20 17:37:15 +0200

    added TODOs

--- src/app/_lib/reducers/types.ts
+++ src/app/_lib/reducers/types.ts
@@ -2,6 +2,7 @@ import { User } from "@/app/(users)/_lib";
 import { Group } from "@/app/groups/_lib";
 import { Item, Receipt } from "@/app/receipts/_lib";
 
+// TODO: refactor receipts related under something similar to `.receipts`
 export interface CliGroup extends Group {
   activeReceipt?: Receipt & {
     focusedItemId?: Item["id"];
